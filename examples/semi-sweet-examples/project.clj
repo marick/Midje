71df79dddce69f88d930c598803d1048713e44b1
Brian Marick <marick@exampler.com>
2010-06-30 15:09:56 -0500

    Bumped to 0.1.1

--- examples/semi-sweet-examples/project.clj
+++ examples/semi-sweet-examples/project.clj
@@ -1,7 +1,7 @@
-(defproject semi-sweet-simple "0.1.0"
+(defproject semi-sweet-simple "0.2.0"
   :description "An example of using Midje semi-sweet mocking"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[midje "0.1.0"]]
+  :dev-dependencies [[midje "0.1.1"]]
 )
 
