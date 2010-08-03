c0637dc80c8ece083de5a53ce4b4697c95677ad0
Wilkes Joiner <wilkesjoiner@gmail.com>
2010-08-03 13:33:51 -0500

    update project.clj for clojure 1.2

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject midje "0.2.0"
+(defproject midje "0.2.0-clojure-1.2"
   :description "A mock/stubbing library for Clojure"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]])
+  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
+                 [org.clojure/clojure-contrib "1.2.0-RC1"]])
 
 ;  :dev-dependencies [[lein-clojars "0.5"]]
 ;  :repositories [["clojars" "http://clojars.org/repo"]])
