0d4ad9ea89d017709b88425e70bbe036daf073d9
Brian Marick <marick@exampler.com>
2010-09-14 16:43:06 -0500

    Updated to 0.5.0

--- examples/semi-sweet-examples/project.clj
+++ examples/semi-sweet-examples/project.clj
@@ -1,7 +1,7 @@
-(defproject semi-sweet-simple "0.4.0"
+(defproject semi-sweet-simple "0.5.0"
   :description "An example of using Midje semi-sweet mocking"
   :dependencies [[org.clojure/clojure "[1.1.0,1.2.0]"]
                  [org.clojure/clojure-contrib "[1.1.0,1.2.0]"]]
-  :dev-dependencies [[midje "0.4.0"]]
+  :dev-dependencies [[midje "0.5.0"]]
 )
 
--- examples/sweet-examples/basic/project.clj
+++ examples/sweet-examples/basic/project.clj
@@ -2,6 +2,6 @@
   :description "An example of using Midje's sweet interface"
   :dependencies [[org.clojure/clojure "[1.1.0,1.2.0]"]
                  [org.clojure/clojure-contrib "[1.1.0,1.2.0]"]]
-  :dev-dependencies [[midje "0.4.0"]]
+  :dev-dependencies [[midje "0.5.0"]]
 )
 
