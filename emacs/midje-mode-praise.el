7e7821782e48a20963e492117610f65264711bb0
Brian Marick <marick@exampler.com>
2010-09-19 15:26:41 -0500

    Fiddling with quotes.

--- emacs/midje-mode-praise.el
+++ emacs/midje-mode-praise.el
@@ -6,7 +6,6 @@
 (defvar midje-praise-quotes 
 '(
   "When someone asks you if you're a god, you say 'YES'! -- Zeddemore" 
-  "Where there is no difficulty there is no praise. -- Johnson"
   "The work itself praises the master. -- CPE Bach"
   "Happiness comes when you believe that you have done something truly meaningful. -- Yan"
   "I do believe in praising that which deserves to be praised. -- Dean Smith"
@@ -17,7 +16,6 @@
   "Without work, all life goes rotten. -- Camus"
   "Work joyfully and peacefully, knowing that right thoughts and right efforts will inevitably bring about right results. -- Allen"
   "Work is either fun or drudgery. It depends on your attitude. I like fun. -- Barrett"
-  "Find something you love to do and you'll never have to work a day in your life -- MacKay"
   "There's a certain satisfaction in a little bit of pain. -- Madonna"
   "One small test for a codebase, one giant leap for quality kind! -- @zspencer"
   "Hey! You're green! Refactor then we get to go red again! -- @zspencer"
@@ -25,10 +23,18 @@
   "Ding! You Have Gained Experience -- @zspencer"
   "A journey of a thousand miles begins with a single step. -- @alanmstokes"
   "If this isn't nice, I don't know what is. -- Vonnegut"
-  "With enough code, all eyes are shallow. -- Jared Richardson"
-  "Simplicity, carried to the extreme, becomes elegance. – Jon Franklin"
+  "Simplicity, carried to the extreme, becomes elegance. -- Jon Franklin"
   "This is the future you were hoping for. -- @Vaguery"
   "O frabjous day! Callooh! Callay! -- Lewis Carroll"
+  "Whoever wants to reach a distant goal must take small steps. -- fortune cookie"
+  "The next function taunts you still. Will you rise to the challenge? -- anonymous"
+  "Any intelligent fool can make things bigger, more complex, and more violent. It takes a touch of genius -- and a lot of courage -- to move in the opposite direction. -- Schumacher"
+  "Excellence is not an act but a habit. -- Aristotle"
+  "Code you'd be proud to give your mom to show off on the fridge. -- Mike Cohn"
+  "A clean boundary between useful abstractions and the grubby code that touches the real world is always a good thing. -- Ron Jeffries"
+  "Of course the universe *is* out to get us, but it's not going to do it by passing a null to one of our methods. -- Ron Jeffries"
+  "Good code is its own best documentation. -- Steve McConnell"
+  "First they ignored you, then they laughed at you, then they fought you, now you've won. -- Not quite Gandhi"
 ))
 
 (provide 'midje-mode-praise)
