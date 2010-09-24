b3be3b661eddbd3ca7f2c026fc37168b6add619a
Brian Marick <marick@exampler.com>
2010-09-24 17:38:12 -0500

    Hold on to some old bug reports as tests.

---
+++ test/midje/sweet/t_canary_tests.clj
@@ -0,0 +1,44 @@
+;;; Not wildly important, these tests are older descriptions of
+;;; bugs. They've been fixed, and more specific tests have been
+;;; written, but no harm in keeping them around. Delete if they start
+;;; to annoy.
+
+
+(ns midje.sweet.t-canary-tests
+  (:use clojure.test)
+  (:use [midje.sweet] :reload-all)
+  (:use [midje.test-util]))
+
+
+(unfinished favorite-animal)
+(defn favorite-animal-name [] (name (favorite-animal)))
+(defn favorite-animal-empty [] )
+(defn favorite-animal-only-animal [] (favorite-animal))
+(defn favorite-animal-only-name [] (name "fred"))
+
+(fact
+     (favorite-animal-name) => "betsy"
+     (provided
+       (name (favorite-animal)) => "betsy"))
+			  
+
+
+
+
+
+(unfinished all-procedures)
+(def ...procedure... '...procedure...)
+(defn build-map [exclusion-function]
+  (reduce (fn [accumulator name] (conj accumulator {name (exclusion-function name)}))
+	  {}
+	  (all-procedures)))
+(defn all-procedures-exclude-nothing []
+  (build-map (fn [procedure] [])))
+
+ (fact "unless otherwise stated, each procedure excludes no animals"
+   (all-procedures-exclude-nothing) => { ...procedure... [] }
+   (provided
+     (all-procedures) => [...procedure...] ))
+  
+
+
