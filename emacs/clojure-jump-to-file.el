1f546cc6a4b21344ed77fb3228561c460ef5823f
Brian Marick <marick@exampler.com>
2010-09-26 15:15:25 -0500

    pull jumping to file into its own file

--- emacs/clojure-jump-to-file.el
+++ emacs/clojure-jump-to-file.el
@@ -1,65 +1,22 @@
-;;; These are some support routines I used before I switched to
-;;; Swank. The good stuff has been moved to
-;;; clojure-jump-to-file.el. I'll keep this around for a while.
-
-
 (require 'clojure-mode)
-(provide 'midje-support)
-(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
 
-(defvar midje-root (expand-file-name "~"))
+(defvar midje-root nil)
 (defvar midje-filename-stash '())
-(defvar midje-inferior-lisp-program "lein repl")
-
-(defun midje-clojure-mode-hook ()
-  (local-set-key "\^c\^c" 'midje-eval-defun-and-go-to-end)
-  (local-set-key "\^c\^v" 'midje-eval-defun-and-run-tests)
-)
-(add-hook 'clojure-mode-hook 'midje-clojure-mode-hook)
-
-
-(defun midje (here)
-  "Start the inferior Clojure process in given directory.
-Enable Midje support commands."
-  (interactive "DDirectory: ")
-  (with-temp-buffer
-    (midje-root here)
-    (setq default-directory here)
-    (setq clojure-inferior-lisp-program midje-inferior-lisp-program)
-    (if (get-buffer "*inferior-lisp*") (kill-buffer "*inferior-lisp*"))
-    ;; TODO: figure out how to get rid of double-echo.
-    (inferior-lisp clojure-inferior-lisp-program))
-)
+(global-set-key "\^hj" 'midje-visit-source)    
 
 (defun midje-root (here)
   "Set the root directory that."
-  (interactive "DDirectory: ")
+  (interactive "DProject Root Directory: ")
   ;; This wants to work in all buffers (or all shell buffers)?
-  (global-set-key "\^h\^h" 'midje-visit-source)    
   (setq midje-root (expand-file-name here))
   (setq midje-filename-stash '()))
 
-
-
-(defun midje-eval-defun-and-go-to-end ()
-  (interactive)
-  (display-buffer "*inferior-lisp*" 'not-this-window)
-  (lisp-eval-defun)
-  (midje-show-end-of-inferior-lisp-buffer)
-)
-
-(defun midje-eval-defun-and-run-tests ()
-  (interactive)
-  (display-buffer "*inferior-lisp*" 'not-this-window)
-  (lisp-eval-defun)
-  (comint-send-string (inferior-lisp-proc) "(run-tests)\n")
-  (midje-show-end-of-inferior-lisp-buffer)
-)
 
 (defun midje-visit-source ()
   "If the current line contains text like '../src/program.clj:34', visit 
 that file in the other window and position point on that line."
   (interactive)
+  (unless midje-root (call-interactively #'midje-root))
   (let* ((start-boundary (save-excursion (beginning-of-line) (point)))
          (regexp (concat "\\([ \t\n\r\"'([<{]\\|^\\)" ; non file chars or
                                                       ; effective
@@ -80,15 +37,6 @@ that file in the other window and position point on that line."
 	  (midje-goto file line increment))
       (error "No Clojure location on line."))))
 
-
-
-
-
-(defun midje-show-end-of-inferior-lisp-buffer ()
-  (let ((home (selected-window)))
-    (select-window (get-buffer-window "*inferior-lisp*"))
-    (goto-char (point-max))
-    (select-window home)))
 
 
 (defun midje-reload-filename-stash (dir)
@@ -128,3 +76,4 @@ that file in the other window and position point on that line."
       (buffer-substring (match-beginning n) (match-end n))
     nil))
 
+(provide 'clojure-jump-to-file)
