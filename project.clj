03dfd29bfa4b27eb7a831241495ae7f7ec77a2c3
Brian Marick <marick@exampler.com>
2010-11-18 13:39:28 -0600

    Forgot to remove Clojure 1.1 from project file

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject midje "0.8.0"
   :description "A TDD library for Clojure, with an emphasis on mocks"
-  :dependencies [[org.clojure/clojure "[1.1.0,1.2.0]"]
-                 [org.clojure/clojure-contrib "[1.1.0,1.2.0]"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
 		 [unifycle "0.5.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
 
