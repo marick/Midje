02250a5c615f2bdc8b70f30cc9358ff3b6dc278f
Brian Marick <marick@exampler.com>
2010-12-07 13:33:34 -0600

    printing with nested indentation.

--- src/midje/util/debugging.clj
+++ src/midje/util/debugging.clj
@@ -1,7 +1,21 @@
-(ns midje.util.debugging)
+(ns midje.util.debugging
+  (:require [clojure.contrib.string :as str]))
+
+(def indent-count (atom 0))
+(def indent (atom ""))
+
+(defn p [& tags]
+  (apply (partial println @indent) tags))
+
+(defn p+ [& tags]
+  (swap! indent-count inc)
+  (swap! indent #(str (str/butlast 1 %) @indent-count ">"))
+  (apply p tags))
 
 (defn nopret [val] val)
 (defn pret [val]
-  (println val)
+  (p val)
+  (swap! indent-count dec)
+  (swap! indent #(str (str/butlast 2 %) ">"))
   val)
 
