9d876497d4807fa5749a784a82bc1c21e53fd456
Brian Marick <marick@exampler.com>
2010-12-09 16:06:48 -0600

    0.8.1

--- examples/sweet-examples/basic/project.clj
+++ examples/sweet-examples/basic/project.clj
@@ -2,6 +2,6 @@
   :description "An example of using Midje's sweet interface"
   :dependencies [[org.clojure/clojure "[1.1.0,1.2.0]"]
                  [org.clojure/clojure-contrib "[1.1.0,1.2.0]"]
-		 [midje "0.8.0"]]
+		 [midje "0.8.1"]]
 )
 
