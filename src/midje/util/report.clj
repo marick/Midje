b93b5690c836a65ed713533bc34c34f799053289
Brian Marick <marick@exampler.com>
2010-12-16 12:09:51 -0600

    pr-str chatty checker intermediate results; finished isolating clojure.test

--- src/midje/util/report.clj
+++ src/midje/util/report.clj
@@ -5,41 +5,16 @@
         (defonce old-report clojure.test/report))))
 
 (ns midje.util.report
-  (:use clojure.test))
+  (:use clojure.test
+	[midje.util.form-utils :only [flatten-and-remove-nils]]))
+
+(def *renderer* println)
 
 (defn- midje-position-string [position-pair]
   (format "(%s:%s)" (first position-pair) (second position-pair)))
 
-;; NOTE: Gradually moving this behavior down to use render. See bottom of file.
-(defmethod clojure.test/old-report :mock-argument-match-failure [m]
-   (inc-report-counter :fail)
-   (println "\nFAIL at" (midje-position-string (:position m)))
-   (when (seq *testing-contexts*) (println (testing-contexts-str)))
-   (println "You never said" (:name (meta (:function m))) "would be needed with these arguments:")
-   (println (pr-str (:actual m))))
-
-(defmethod clojure.test/old-report :mock-incorrect-call-count [m]
-   (inc-report-counter :fail)
-   (println "\nFAIL for" (midje-position-string (:position m)))
-   (when (seq *testing-contexts*) (println (testing-contexts-str)))
-   (println "You claimed the following was needed, but it was never used:")
-   (println (:expected m)))
-
-
-(defmethod clojure.test/old-report :mock-expected-result-failure [m]
-   (inc-report-counter :fail)
-   (println "\nFAIL at" (midje-position-string (:position m)))
-   (when (seq *testing-contexts*) (println (testing-contexts-str)))
-   (println "Expected:" (pr-str (:expected m)))
-   (println "  Actual:" (pr-str (:actual m))))
-
-(def *renderer* println)
-
 (defmacro with-identity-renderer [& forms]   ; for testing
   `(binding [*renderer* identity] ~@forms))
-
-(defn flatten-and-remove-nils [seq]
-  (filter identity (flatten seq)))
 
 (defn- without-nasty-looking-functions [form]
   (if-let [name (and (fn? form)
@@ -47,18 +22,39 @@
     (format "a function named '%s'" name)
     (pr-str form)))
 
+(defn- fail-at [m]
+  (str "\nFAIL at " (midje-position-string (:position m))))
+  
+
 (defmulti report-strings :type)
+
+(defmethod report-strings :mock-argument-match-failure [m]
+   (list
+    (fail-at m)
+    (str "You never said " (:name (meta (:function m))) " would be needed with these arguments:")
+    (str (pr-str (:actual m)))))
+
+(defmethod report-strings :mock-incorrect-call-count [m]
+   (list
+    (fail-at m)
+    (str "You claimed the following was needed, but it was never used:")
+    (str (:expected m))))
+
+(defmethod report-strings :mock-expected-result-failure [m]
+   (list
+    (fail-at m)
+    (str "Expected: " (pr-str (:expected m)))
+    (str "  Actual: " (pr-str (:actual m)))))
 
 (defmethod report-strings :mock-expected-result-functional-failure [m]
   (list
-   (str "\nFAIL at " (midje-position-string (:position m)))
-   (when (seq *testing-contexts*) (testing-contexts-str))
+   (fail-at m)
    "Actual result did not agree with the checking function."
    (str "    Actual result: " (without-nasty-looking-functions (:actual m)))
    (str "Checking function: " (pr-str (:expected m)))
    (if (:intermediate-results m)
      (cons "During checking, these intermediate values were seen:"
-	   (map (fn [[form value]] (str "   " form " => " value))
+	   (map (fn [[form value]] (str "   " (pr-str form) " => " (pr-str value)))
 		(:intermediate-results m))))))
 
 (defmethod report-strings :future-fact [m]
@@ -70,7 +66,7 @@
 (defn render [m]
   (doall (map *renderer* (flatten-and-remove-nils (report-strings m)))))
 
-(defmethod clojure.test/old-report :mock-expected-result-functional-failure [m]
+(defmethod clojure.test/old-report :default [m]
    (inc-report-counter :fail)
    (render m))
 
--- test/midje/util/t_report_test.clj
+++ test/midje/util/t_report_test.clj
@@ -1,5 +1,6 @@
 (ns midje.util.t-report-test
   (:use [midje.util.report] :reload-all)
+  (:use [midje.sweet])
   (:use [clojure.test])
   (:use [midje.test-util]))
 
@@ -7,45 +8,82 @@
 (testable-privates midje.util.report midje-position-string functional-failure-lines
 		   without-nasty-looking-functions)
 
-(deftest position-string-test
-  (is (= (midje-position-string ["filename.clj" 33])
-	 "(filename.clj:33)")))
+(fact "string positions have filenames and line numbers"
+  (midje-position-string ["filename.clj" 33]) => "(filename.clj:33)")
+
+(defn re [expected]
+  (fn [actual] (re-find expected actual)))
+
+;; In the case of the checker (exactly odd?), you want to see failures
+;; written in terms of a function name instead of some absurdly complicated
+;; #<core$even_QMARK_ clojure.core$even_QMARK_@15ee9cc3>
+(fact "(exactly odd?) is printed attractively"
+  (without-nasty-looking-functions even?) => "a function named 'even?'"
+  (without-nasty-looking-functions (fn [n] 1)) => (re #"fn__"))
 
 
-(deftest flatten-and-remove-nils-test
-  (is (= [] (flatten-and-remove-nils '())))
-  (is (= ["foo" "bar" "baz"]
-	   (flatten-and-remove-nils '(nil "foo" ("bar" nil "baz"))))))
-
-(deftest functional-failure-renderer-test
+(fact "rendering functional failures"
   (let [failure-map {:type :mock-expected-result-functional-failure
 		     :actual 2
 		     :intermediate-results [ ['(f 1) 33] ]
 		     :position ["foo.clj" 3]
 		     :expected '(test-checker 33)}
-	raw-report (with-identity-renderer (render failure-map))]
-    (is (re-find #"FAIL.*foo.clj:3" (nth raw-report 0)))
-    (is (re-find #"Actual.*did not agree" (nth raw-report 1)))
-    (is (re-find #"Actual.*2" (nth raw-report 2)))
-    (is (re-find #"Checking function.*test-checker 33" (nth raw-report 3)))
-    (is (re-find #"intermediate values" (nth raw-report 4)))
-    (is (re-find #"\(f 1\) => 33" (nth raw-report 5))))
+	raw-report (with-identity-renderer (clojure.test/old-report failure-map))]
+
+    (nth raw-report 0) => (re #"FAIL.*foo.clj:3")
+    (nth raw-report 1) => (re #"Actual.*did not agree")
+    (nth raw-report 2) => (re #"Actual.*2")
+    (nth raw-report 3) => (re #"Checking function.*test-checker 33")
+    (nth raw-report 4) => (re #"intermediate values")
+    (nth raw-report 5) => (re #"\(f 1\) => 33"))
     
   (let [failure-map {:type :mock-expected-result-functional-failure
 		     :actual 2
 		     :position ["foo.clj" 3]
 		     :expected 'odd?}
-	raw-report (with-identity-renderer (render failure-map))]
-    (is (re-find #"FAIL.*foo.clj:3" (nth raw-report 0)))
-    (is (re-find #"Actual.*did not agree" (nth raw-report 1)))
-    (is (re-find #"Actual.*2" (nth raw-report 2)))
-    (is (re-find #"Checking function.*odd?" (nth raw-report 3)))))
+	raw-report (with-identity-renderer (clojure.test/old-report failure-map))]
+    (nth raw-report 0) => (re #"FAIL.*foo.clj:3")
+    (nth raw-report 1) => (re #"Actual.*did not agree")
+    (nth raw-report 2) => (re #"Actual.*2")
+    (nth raw-report 3) => (re #"Checking function.*odd?"))
 
-;; In the case of the checker (exactly odd?), you want to see failures
-;; written in terms of a function name instead of some absurdly complicated
-;; #<core$even_QMARK_ clojure.core$even_QMARK_@15ee9cc3>
+  "values in strings are formatted via pr-str"
+  (let [failure-map {:type :mock-expected-result-functional-failure
+	       :actual nil
+	       :expected '(sloobom "forp")
+	       :intermediate-results [['(+ 1 "ate") nil]]
+	       :position ["foo" 23]}
+	raw-report (with-identity-renderer (clojure.test/old-report failure-map))]
+    (nth raw-report 2) => (re #"result: nil")
+    (nth raw-report 3) => (re #"function: \(sloobom \"forp\"")
+    (nth raw-report 5) => (re #"\(\+ 1 \"ate\"\) => nil")))
 
-(deftest without-nasty-looking-functions-test
-  (is (= "a function named 'even?'" (without-nasty-looking-functions even?)))
-  (is (re-find #"fn__" (str (without-nasty-looking-functions (fn [n] 1))))))
 
+(fact "excess matches"
+  (let [failure-map {:type :mock-argument-match-failure
+		     :actual '(nil)
+		     :position ["foo.clj" 3]
+		     :function odd?}
+	raw-report (with-identity-renderer (clojure.test/old-report failure-map))]
+    (nth raw-report 0) => (re #"FAIL at .*foo.clj:3")
+    (nth raw-report 1) => (re #"never said odd\? would be needed")
+    (nth raw-report 2) => "(nil)"))
+
+(fact "mock called an incorrect number of times"
+  (let [failure-map {:type :mock-incorrect-call-count
+		     :position ["foo.clj" 3]
+		     :expected "(f a)"}
+	raw-report (with-identity-renderer (clojure.test/old-report failure-map))]
+    (nth raw-report 0) => (re #"FAIL at .*foo.clj:3")
+    (nth raw-report 1) => (re #"claimed the following was needed")
+    (nth raw-report 2) => "(f a)"))
+
+(fact "ordinary bad result from equality"
+  (let [failure-map {:type :mock-expected-result-failure
+		     :position ["foo.clj" 3]
+		     :actual nil
+		     :expected "s"}
+	raw-report (with-identity-renderer (clojure.test/old-report failure-map))]
+    (nth raw-report 0) => (re #"FAIL at .*foo.clj:3")
+    (nth raw-report 1) => (re #"Expected: \"s\"")
+    (nth raw-report 2) => (re #"Actual: nil")))
