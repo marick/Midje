5d919b76356ab7a10a5df63c4c0c0534cc571226
Brian Marick <marick@exampler.com>
2010-12-18 15:31:09 -0600

    Bump development version to 0.8.2

--- examples/sweet-examples/adder-webapp/project.clj
+++ examples/sweet-examples/adder-webapp/project.clj
@@ -10,5 +10,5 @@
      [hiccup "0.2.6"]]
   :dev-dependencies
   [[lein-run "1.0.0"]
-   [midje "0.8.1"]
+   [midje "0.8.2"]
    ])
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject midje "0.8.1"
+(defproject midje "0.8.2"
   :description "A TDD library for Clojure, with an emphasis on mocks"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
