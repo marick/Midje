35adc334da3893ee14b5d131c8894991d319690d
Brian Marick <marick@exampler.com>
2010-12-18 15:30:35 -0600

    Separate out tutorial on compound checkers to make this more appealing.

--- examples/sweet-examples/adder-webapp/test/adder/core_test.clj
+++ examples/sweet-examples/adder-webapp/test/adder/core_test.clj
@@ -2,61 +2,40 @@
   (:use midje.sweet)
   (:use adder.core))
 
-(fact "parse input checks errors"
- (parse-input "1" "2") => [1 2]      
- (parse-input "foo" "2") => (throws NumberFormatException)
-)
+(facts "about utilities"
+  "parse-input"
+  (parse-input "1" "2") => [1 2]      
+  (parse-input "foo" "2") => (throws NumberFormatException)
 
-(defn contains [regexp] (partial re-find regexp))
-
-(fact  ;; Description is not required.
- (view-output 1 2 3) => (contains #"two numbers added")
-)
-
-(let [response (handler {:uri "/" :request-method :get})]
-  (fact "Handles bound symbols as you'd expect"
-   (:status response) => 200
-   (:body response) => (contains #"add two numbers"))
-)
+  "view-output"
+  (view-output 1 2 3) => (contains #"two numbers added"))
 
 ;; Approximately one zillion facts will need to check both the
-;; response code and the content of the resulting body. You'll want
-;; helper functions. Here's one, but it's only virtue is that it does
-;; fail and succeed when it should.
+;; response code and the content of the resulting body, so I make
+;; a custom checker. For other kinds of custom checkers, see
+;; examples/compound-checkers.
 
-(defn awkward-successful-page? [response regexp]
-  (and (= (:status response) 200)
-       ((contains regexp) (:body response))))
+(defmacro successful-page-containing [regexp]
+  `(fn [response#]
+     (and 
+      (expect (:status response#) => 200)
+      (expect (:body response#) => (contains ~regexp)))
+     true))
 
-(fact
- (awkward-successful-page? (handler {:uri "/" :request-method :post :params {"a" "1" "b" "2"}})
-		   #"1 \+ 2 = 4") => truthy
-		   )
+(facts "about the main page"
+  "Bare request produces prompt"
+  (handler {:uri "/" :request-method :get})
+  => (successful-page-containing #"add two numbers")
 
-(println "^^^^ Failure expected. (The test was changed to test the wrong thing.) ^^^^")
+  "Valid numbers produce sum"
+  (handler {:uri "/" :request-method :post :params {"a" "1" "b" "2"}})
+  => (successful-page-containing #"1 \+ 2 = 3")
 
-;; The failure messages are not particularly useful. To get better
-;; ones, place expect statements within the helper. I think it's more
-;; readable to use the helper on the right-hand-side of the statement.
+  "Non-numbers produce error message"
+  (handler {:uri "/" :request-method :post :params {"a" "2" "b" "f"}})
+  => (successful-page-containing #"those are not both numbers"))
 
-(defn successful-page-containing [regexp]
-  (fn [response] 
-    (expect (:status response) => 200)
-    (expect (:body response) => (contains regexp))
-    true))
-
-(let [response (handler {:uri "/" :request-method :post :params {"a" "2" "b" "3"}})]
-  (fact response => (successful-page-containing #"those are not both numbers")))
-
-;; The true at the end of the helper is because expect returns false
-;; in the case of a failure, which would cause a cascading failure in
-;; the caller.
-
-(println "^^^^ Failure expected. (The test was changed to test the wrong thing.) ^^^^")
-
-(let [response (handler {:uri "/anything" :request-method :get})]
-  (fact
-   (:status response) => 302
-   (get-in response [:headers "Location"]) => "/")
-)
-  
+(fact "Other pages produce redirect"
+  (let [response (handler {:uri "/anything" :request-method :get})]
+    (:status response) => 302
+    (get-in response [:headers "Location"]) => "/"))
