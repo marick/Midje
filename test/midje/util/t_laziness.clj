a4c00f6b1069dedd56255cfa3c28f956ac652e33
Brian Marick <marick@exampler.com>
2011-01-02 16:48:55 -0600

    split out laziness test to correspond to src structure

---
+++ test/midje/util/t_laziness.clj
@@ -0,0 +1,24 @@
+(ns midje.util.t-laziness
+  (:use [midje.sweet]
+	[midje.util laziness]
+	[midje.test-util]))
+
+;; Justification for use of eagerly
+(def counter (atom 1))
+(def mocked-function-produces-next-element inc)
+
+(defn function-under-test-produces-a-lazy-list []
+  (iterate mocked-function-produces-next-element 1))
+
+(defn mock-use []
+  (binding [mocked-function-produces-next-element (fn [n] (swap! counter inc) (inc n))]
+    (eagerly (take 5 (function-under-test-produces-a-lazy-list)))))
+
+(fact "eagerly forces evaluation"
+  (mock-use)
+  @counter => 5)
+
+(fact "eagerly allows non-sequences"
+  (eagerly 3) => 3)
+
+
