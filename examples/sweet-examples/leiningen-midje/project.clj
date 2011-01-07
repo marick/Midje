53b0ee35d7990aaae5df84915f6def381a6849f0
Brian Marick <marick@exampler.com>
2011-01-07 10:17:10 -0600

    0.9 final

--- examples/sweet-examples/leiningen-midje/project.clj
+++ examples/sweet-examples/leiningen-midje/project.clj
@@ -2,4 +2,4 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [midje "0.9.0RC1"]])
+		 [midje "0.9.0"]])
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject midje "0.9.0RC1"
+(defproject midje "0.9.0"
   :description "A TDD library for Clojure, with an emphasis on mocks"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
