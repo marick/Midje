7e4ee948e996208b077662117711b7c50794a362
Brian Marick <marick@exampler.com>
2011-01-09 16:54:11 -0600

    A working cake task for running midje+clojure.test

--- examples/sweet-examples/cake-midje/src/cake_midje/core.clj
+++ examples/sweet-examples/cake-midje/src/cake_midje/core.clj
@@ -1,1 +1,7 @@
-(ns cake-midje.core)
+(ns cake-midje.core
+  (:use midje.sweet))
+
+(println "\n+++ The following shows how 'cake midje' checks facts in source files.")
+(println "+++ The failure is intentional.")
+(fact
+  (+ 1 2) => "I am a source-file fact.")
--- examples/sweet-examples/cake-midje/test/cake_demo/t_core.clj
+++ examples/sweet-examples/cake-midje/test/cake_demo/t_core.clj
@@ -3,6 +3,11 @@
   (:use midje.sweet))
 
 (deftest a-clojure-test-test
-  (is (= 1 23)))
+  (println "\n+++ The following shows how 'cake midje' runs clojure.test deftests.")
+  (println "+++ The failure is intentional.")
+  (is (= 1 "I am a deftest expected result.")))
 
-(fact 1 => 3)
+(println "\n+++ The following shows how 'cake midje' checks facts in test files.")
+(println "+++ The failure is intentional.")
+(fact
+  (+ 1 2) => "I am a test file fact")
