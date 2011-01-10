0efb32b7fe584f58cd5e3bb96202d521c29730d4
Brian Marick <marick@exampler.com>
2011-01-10 12:06:09 -0600

    Improved examples & their documentation.

--- examples/leiningen-midje/test/leiningen_midje/t_core.clj
+++ examples/leiningen-midje/test/leiningen_midje/t_core.clj
@@ -1,10 +1,14 @@
 (ns leiningen-midje.t-core
-  (:use [leiningen-midje.core] :reload)
-  (:use [midje.sweet]))
+  (:use leiningen-midje.core
+	clojure.test
+	midje.sweet))
 
-
-
+(println "The following two failures are intentional.")
 (fact
   (f 1) => 1
   (provided
     (g 1) => 1))
+
+(deftest a-failing-test
+  (println "The following failure (from deftest) is intentional.")
+  (is (= 33 (+ 1 2))))
