7f00b6f5912995da8a93390cf45b554062f1bc71
Brian Marick <marick@exampler.com>
2011-01-13 14:47:39 -0600

    /collector's/ edition, you dolt\!

--- examples/leiningen-test/project.clj
+++ examples/leiningen-test/project.clj
@@ -2,4 +2,4 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [midje "1.0.0-COMMEMORATIVE-EDITION"]])
+		 [midje "1.0.0-COLLECTORS-EDITION"]])
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject midje "1.0.0-COMMEMORATIVE-EDITION"
+(defproject midje "1.0.0-COLLECTORS-EDITION"
   :description "A TDD library for Clojure, with an emphasis on mocks"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
