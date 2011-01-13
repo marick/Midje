096c80f0f53021426c7ebc6d8d782703d423a7c0
Brian Marick <marick@exampler.com>
2011-01-13 14:14:18 -0600

    Changed to use collection checkers.

--- examples/adder-webapp/test/adder/core_test.clj
+++ examples/adder-webapp/test/adder/core_test.clj
@@ -10,32 +10,23 @@
   "view-output"
   (view-output 1 2 3) => (contains #"two numbers added"))
 
-;; Approximately one zillion facts will need to check both the
-;; response code and the content of the resulting body, so I make
-;; a custom checker. For other kinds of custom checkers, see
-;; examples/compound-checkers.
-
-(defmacro successful-page-containing [regexp]
-  `(fn [response#]
-     (and 
-      (expect (:status response#) => 200)
-      (expect (:body response#) => (contains ~regexp)))
-     true))
-
 (facts "about the main page"
   "Bare request produces prompt"
   (handler {:uri "/" :request-method :get})
-  => (successful-page-containing #"add two numbers")
+  => (contains {:status 200
+                :body #"add two numbers"})
 
   "Valid numbers produce sum"
   (handler {:uri "/" :request-method :post :params {"a" "1" "b" "2"}})
-  => (successful-page-containing #"1 \+ 2 = 3")
+  => (contains {:status 200
+                :body #"1 \+ 2 = 3"})
 
   "Non-numbers produce error message"
   (handler {:uri "/" :request-method :post :params {"a" "2" "b" "f"}})
-  => (successful-page-containing #"those are not both numbers"))
+  => (contains {:status 200
+                :body #"those are not both numbers"}))
 
 (fact "Other pages produce redirect"
-  (let [response (handler {:uri "/anything" :request-method :get})]
-    (:status response) => 302
-    (get-in response [:headers "Location"]) => "/"))
+  (handler {:uri "/anything" :request-method :get})
+  => (contains {:status 302,
+                :headers (contains {"Location" "/"})}))
