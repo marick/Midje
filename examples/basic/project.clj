b6d59dd3feeead3848d22d44e2e019ff9f612eeb
Brian Marick <marick@exampler.com>
2011-01-17 16:50:54 -0600

    1.0

--- examples/basic/project.clj
+++ examples/basic/project.clj
@@ -2,6 +2,6 @@
   :description "An example of using Midje's sweet interface"
   :dependencies [[org.clojure/clojure "[1.1.0,1.2.0]"]
                  [org.clojure/clojure-contrib "[1.1.0,1.2.0]"]
-		 [midje "1.0.0-COLLECTORS-EDITION"]]
+		 [midje "1.0.0"]]
 )
 
--- examples/leiningen-midje/project.clj
+++ examples/leiningen-midje/project.clj
@@ -2,4 +2,4 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [midje "1.0.0-COLLECTORS-EDITION"]])
+		 [midje "1.0.0"]])
