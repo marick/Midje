febfd46d5919d7b95ef4918947666ad525be9315
Brian Marick <marick@exampler.com>
2011-02-02 11:13:06 -0600

    Preparation for multi-body checkers

--- src/midje/checkers/defining.clj
+++ src/midje/checkers/defining.clj
@@ -5,7 +5,9 @@
 (defn tag-as-checker [function]
   (vary-meta function merge {:midje/checker true}))
 
+
 (defn- make-checker-definition [checker-name docstring args body]
+;  (prn checker-name docstring args body)
   (let [core-function `(fn ~(vec args) ~@body)
         core-metavars {:midje/checker true :arglists `'~(list args)}
         metavars-to-add (if docstring
@@ -15,12 +17,20 @@
           (.withMeta ~core-function
                      (assoc (.meta (var ~checker-name)) :midje/checker true)))))
 
+(defn- working-with-body-and-args [checker-name docstring body-and-args]
+;  (prn checker-name docstring body-and-args)
+  (make-checker-definition checker-name docstring
+                           (first body-and-args) (rest body-and-args)))
+  
+
 (defmacro defchecker [checker-name & stuff]
   "Like defn, but tags the variable created and the function it refers to
    as checkers."
+
+;  (prn "XXXX" [checker-name stuff])
   (if (string? (first stuff))
-    (make-checker-definition checker-name (first stuff) (second stuff) (nthnext stuff 2))
-    (make-checker-definition checker-name nil (first stuff) (nthnext stuff 1))))
+    (working-with-body-and-args checker-name (first stuff) (rest stuff))
+    (working-with-body-and-args checker-name nil stuff)))
     
 (defmacro checker [args & body]
   `(tag-as-checker (fn ~(vec args) ~@body)))
--- test/midje/checkers/t_defining.clj
+++ test/midje/checkers/t_defining.clj
@@ -5,23 +5,50 @@
         [midje.checkers.defining :only [checker?]]
         midje.test-util))
 
-(defchecker magic-number "docstring" [actual] (= 587 actual))
-(fact
+(defchecker magic-number "magic number docstring" [actual] (= 587 actual))
+(fact "checker with a doc string"
   magic-number => checker?
   #'magic-number => checker?
   587 => magic-number
   (magic-number 8) => falsey
-  (:doc (meta #'magic-number)) => "docstring")
+  (:arglists (meta #'magic-number)) => '([actual])
+  (:doc (meta #'magic-number)) => "magic number docstring")
 
 (defchecker undocumented-magic-number [actual] (= 587 actual))
-(fact
+(fact "checker without a docstring"
   undocumented-magic-number => checker?
   #'undocumented-magic-number => checker?
   587 => undocumented-magic-number
   (undocumented-magic-number 8) => falsey
+  (:arglists (meta #'undocumented-magic-number)) => '([actual])
   (:doc (meta #'undocumented-magic-number)) => nil)
 
-(fact
+;; (defchecker double-magic-number "double docstring"
+;;   ([actual] (= 587 actual))
+;;   ([actual increment] (= (+ actual increment) 587)))
+;; (future-fact "checker with a doc string and more than one function body"
+;;   double-magic-number => checker?
+;;   #'double-magic-number => checker?
+;;   587 => double-magic-number
+;;   (double-magic-number 8) => falsey
+;;   (double-magic-number 587 0) => truthy
+;;   (double-magic-number 587 1) => falsey
+;;   (:arglists (meta #'double-magic-number)) => '([actual] [actual increment])
+;;   (:doc (meta #'double-magic-number)) => "double docstring")
+
+;; (defchecker double-magic-number-no-docstring "double docstring"
+;;   ([actual] (= 587 actual))
+;;   ([actual increment] (= (+ actual increment) 587)))
+;; (future-fact "checker with a doc string and more than one function body"
+;;   double-magic-number-no-docstring => checker?
+;;   #'double-magic-number-no-docstring => checker?
+;;   587 => double-magic-number-no-docstring
+;;   (double-magic-number-no-docstring 8) => falsey
+;;   (double-magic-number-no-docstring 587 0) => truthy
+;;   (double-magic-number-no-docstring 587 1) => falsey
+;;   (:arglists (meta #'double-magic-number-no-docstring)) => '([actual] [actual increment]))
+
+(fact "inline checker"
   (let [inline-magic-number (checker [actual] (= 587 actual))]
     inline-magic-number => checker?
     587 => inline-magic-number
