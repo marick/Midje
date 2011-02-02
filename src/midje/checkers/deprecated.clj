66fb5526f7500841a9ccd6f6dc5b09a082959754
Brian Marick <marick@exampler.com>
2011-02-02 11:13:17 -0600

    Some checkers had wrong form.

--- src/midje/checkers/deprecated.clj
+++ src/midje/checkers/deprecated.clj
@@ -11,9 +11,10 @@
 
 ;; Note: checkers need to be exported in ../checkers.clj
 
-(defchecker map-containing [expected]
+(defchecker map-containing 
   "Accepts a map that contains all the keys and values in expected,
    perhaps along with others"
+  [expected]
   (contains expected))
 
 (defn- one-level-map-flatten [list-like-thing]
@@ -21,23 +22,25 @@
     list-like-thing
     (first list-like-thing)))
 
-(defn only-maps-containing [& maps-or-maplist]
+(defn only-maps-containing 
   "Each map in the argument(s) contains some map in the expected
    result. There may be no extra maps in either the argument(s) or expected result.
 
    You can call this with either (only-maps-containing {..} {..}) or
    (only-maps-containing [ {..} {..} ])."
+  [& maps-or-maplist]
   {:midje/checker true}
   (let [expected (one-level-map-flatten maps-or-maplist)
         subfunctions (map contains expected)]
     (just subfunctions :in-any-order)))
   
-(defchecker maps-containing [& maps-or-maplist]
+(defchecker maps-containing 
   "Each map in the argument(s) contains contains some map in the expected
    result. There may be extra maps in the actual result.
 
    You can call this with either (maps-containing {..} {..}) or
    (maps-containing [ {..} {..} ])."
+  [& maps-or-maplist]
   (let [expected (one-level-map-flatten maps-or-maplist)
         subfunctions (map contains expected)]
     (contains subfunctions :in-any-order :gaps-ok)))
