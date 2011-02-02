c885a0e6e20a0bf826bdcbb3b03f74d939543cab
Brian Marick <marick@exampler.com>
2011-02-02 16:33:29 -0600

    Deleted old-fashioned test utilities

--- test/midje/test_util.clj
+++ test/midje/test_util.clj
@@ -28,56 +28,10 @@
      (run-without-reporting (fn [] ~run-form))
      ~@check-forms))
 
-(defmacro one-case 
-  ([description]
-   `(println "PENDING:"  ~description))
-  ([description expect-form & check-forms]
-   (let [form-is-expect? (fn [form] (and (seq? form)
-                                         (= (first form) 'expect)))]
-     (assert (form-is-expect? expect-form))
-     (assert (every? #(not (form-is-expect? %)) check-forms))
-     (run-and-check expect-form check-forms))))
-
-
 (defmacro after-silently
   [example-form & check-forms]
   (run-and-check example-form check-forms))
 
-
-(defn reported?
-  ([expected-count]
-     (reported? expected-count {}))
-  ([expected-count expected-maps]
-     (if (= expected-count (count @reported))
-       (let [one-expected-matches-one-reported?
-             (fn [expected reported] (subset? (set expected) (set reported)))
-
-             one-expected-matches-any-reported?
-             (fn [expected reported-seq]
-               (some #(one-expected-matches-one-reported? expected %) reported-seq))
-
-             all-expected-match-some-reported?
-             (fn [expected-seq reported-seq]
-               (every? (fn [expected]
-                         (one-expected-matches-any-reported? expected reported-seq))
-                       expected-seq))]
-         (if (all-expected-match-some-reported? expected-maps @reported)
-           true
-           (do
-             (println "Expected not a subset of actual.")
-             (println "Expected" expected-maps)
-             (println "Actual" @reported)
-             false)))
-       (do
-         (println "Count" (count @reported) "when" expected-count "expected")
-         (println @reported)
-         false))))
-  
-  
-(defn only-one-result? []
-  (reported? 1))
-(defn no-failures? []
-  (every? #(= (:type %) :pass) @reported))
 
 (defn only-passes? [expected-count]
   (cond (not (= (count @reported) expected-count))
