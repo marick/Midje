f3ce6d15bda2a8357f46f99ff2cb2314e05fc3de
Brian Marick <marick@exampler.com>
2011-02-05 15:55:28 -0600

    Adding Marginalia. (Doesn't work yet.)

--- examples/basic/project.clj
+++ examples/basic/project.clj
@@ -3,5 +3,5 @@
   :dependencies [[org.clojure/clojure "[1.1.0,1.2.0]"]
                  [org.clojure/clojure-contrib "[1.1.0,1.2.0]"]
 		 [midje "1.1-alpha-1"]]
-)
+  :dev-dependencies [[marginalia "0.3.2"]])
 
