84eacbf8ce1dfd2beff385b94000eb6b87fa8f07
Brian Marick <marick@exampler.com>
2011-02-05 15:56:14 -0600

    Added future-fact to pass on chatty results further.

--- test/midje/t_checkers.clj
+++ test/midje/t_checkers.clj
@@ -33,5 +33,24 @@
   [1] => (has every? odd?))
     
 
-  
 
+(defn as-sets [& expected]
+  (let [set-of-sets #(set (map set %))]
+    (fn [actual]
+      ( (just (set-of-sets expected)) (set-of-sets actual)))))
+
+(after-silently 
+ (fact 
+   [ [1] [2 3] ] => (as-sets [ [1] ]))
+ (future-fact "Failures from chatty-checkers-within-functions propagate chatty information"
+   (first @reported) => (contains {:type :mock-expected-result-functional-failure
+                                   :actual [ [1] [2 3]]
+                                   :expected '(as-sets [[1]])
+                                   :notes  ["Expected one element. There were two."]})))
+
+
+
+
+
+   
+ 
