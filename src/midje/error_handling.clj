44fcb37854fa61f6f6721cc434e679e8420ce84f
Brian Marick <marick@exampler.com>
2011-02-28 16:52:56 -0600

    A variant Maybe monad

--- src/midje/error_handling.clj
+++ src/midje/error_handling.clj
@@ -2,8 +2,38 @@
 
 (ns midje.error-handling
   (:use [clojure.contrib.pprint :only [cl-format]]
+        [clojure.contrib.monads]
         [midje.util report file-position form-utils]
         [clojure.test]))
+
+;; My own maybe monad
+
+(defn as-user-error [form]
+  (vary-meta form assoc :midje-user-error true))
+
+(defn user-error-form? [form]
+  (:midje-user-error (meta form)))
+
+(defn user-error-report-form [message position]
+  (as-user-error `(report {:type :user-error
+                           :message ~message
+                           :position ~position})))
+
+; Maybe monad
+(defmonad midje-maybe-m
+   "Monad describing form processing with possible failures. Failure
+   is represented by any form with metadata :midje-user-error"
+   [m-result identity
+    m-bind   (fn [mv f] (if (user-error-form? mv) mv (f mv)))
+    ])
+
+(defmacro error-let [let-vector & body]
+  `(domonad midje-maybe-m [~@let-vector] ~@body))
+(defmacro safely [fn & body]
+  `( (with-monad midje-maybe-m (m-lift ~(count body) ~fn))
+     ~@body))
+
+
 
 (defn make-broken-fake [overrides & message-args]
   (merge `{:type :broken-fake
--- test/midje/t_error_handling.clj
+++ test/midje/t_error_handling.clj
@@ -1,7 +1,45 @@
 ;; -*- indent-tabs-mode: nil -*-
 
 (ns midje.t-error-handling
-  (:use [midje sweet error-handling test-util]))
+  (:use [midje sweet error-handling test-util]
+        [clojure.contrib monads]))
+
+(fact "any form can be turned into a user-error form"
+  (meta (as-user-error '(form))) => (contains {:midje-user-error true})
+  (as-user-error '(form)) => user-error-form?)
+
+(def my-favorite-error-form (as-user-error '(error form)))
+
+(fact "there is an error monad for Midje"
+  (domonad midje-maybe-m
+           [a 1
+            b (inc a)]
+           b) => 2
+
+  (let [result (domonad midje-maybe-m
+                        [a my-favorite-error-form
+                         b (inc a)]
+                        b)]
+    result => my-favorite-error-form
+    result => user-error-form?))
+
+(fact "there is syntactic sugar for it"
+  (error-let [a my-favorite-error-form
+              b (inc a)]
+    b) => my-favorite-error-form)
+
+(fact "safely turns a function application into one that propagates errors."
+  (safely concat my-favorite-error-form '()) => my-favorite-error-form)
+
+(fact "there is a helper function that produces error-reporting forms"
+  (let [result (user-error-report-form "message" "position")]
+    result => '(clojure.test/report {:type :user-error
+                                     :message "message"
+                                     :position "position" })
+    result => user-error-form?))
+
+
+
 
 ;;Util
 
