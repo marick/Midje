3db03601b1a72a026c24cd73f11521eeb0947d6e
Brian Marick <marick@exampler.com>
2011-03-23 16:20:17 -0500

    Add midje-test-only arrow form.

--- src/midje/semi_sweet.clj
+++ src/midje/semi_sweet.clj
@@ -14,7 +14,10 @@
 (def => "=>")
 (def =not=> "=not=>")
 (def =deny=> "=deny=>")
-(def =streams=> "=streams=>")   
+(def =streams=> "=streams=>")
+
+(def ^{:doc "Use this when testing Midje code that processes arrow-forms"}
+     =test=> "=test=>")
 
 (def expect-arrows [=> =not=> =deny=>])
 (def fake-arrows [=> =streams=>])
