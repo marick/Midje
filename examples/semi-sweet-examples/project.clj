5b8fc5aa2999f900d47e0a84bf5c230cecdd6087
Brian Marick <marick@exampler.com>
2011-04-29 17:57:01 -0500

    1.1.1

--- examples/semi-sweet-examples/project.clj
+++ examples/semi-sweet-examples/project.clj
@@ -2,7 +2,7 @@
   :description "An example of using Midje semi-sweet mocking"
   :dependencies [[org.clojure/clojure "[1.1.0,1.2.0]"]
                  [org.clojure/clojure-contrib "[1.1.0,1.2.0]"]
-		 [midje "1.1"]
+		 [midje "1.1.1"]
 		 ]
 )
 
