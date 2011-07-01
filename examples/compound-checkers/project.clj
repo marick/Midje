71f650a141c74426205d2a90049d6dc37f0b2d47
Brian Marick <marick@exampler.com>
2011-07-01 17:22:13 -0500

    lein-midje is now its own project.

--- examples/compound-checkers/project.clj
+++ examples/compound-checkers/project.clj
@@ -1,6 +1,7 @@
 (defproject compound "0.4.0"
   :description "Compound checker"
   :dependencies
-    [[org.clojure/clojure "[1.2.0,1.2.1]"]
-     [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[midje "1.2-alpha4"]])
+  [[org.clojure/clojure "[1.2.0,1.2.1]"]
+   [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[midje "1.2-alpha4"]
+                     [lein-midje "[1.0.0,)"]])
