fa6542053a6210d6b3782023ec298cde532408ba
fogus <mefogus@gmail.com>
2011-07-05 16:10:15 -0400

    Added bits and pieces for core.unify mojo

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "A TDD library for Clojure, with an emphasis on mocks"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [unifycle "0.5.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
-
+		 [unifycle "0.5.0"]
+                 [org.clojure/core.unify "0.5.1-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :repositories {"sonatype-oss-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"})
