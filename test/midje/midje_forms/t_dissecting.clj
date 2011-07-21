93fb47a63903b42c832c2d948aef2122e96f48d4
Brian Marick <marick@exampler.com>
2011-07-20 21:52:28 -0500

    Tabularized tests

--- test/midje/midje_forms/t_dissecting.clj
+++ test/midje/midje_forms/t_dissecting.clj
@@ -15,17 +15,24 @@
 (expect (separate-background-forms '[ (against-background) (f 1) => 3 ]) => [ [] '[ (f 1) => 3 ] ])
 
 
-(fact "separate-background-forms divides forms into background and other things"
-  (separate-background-forms []) =>
-                 [ [] [] ]
-  (separate-background-forms '[ (f 1) => 3 ]) =>
-             [ [] '[ (f 1) => 3 ] ]
-  (separate-background-forms '[ (against-background) (f 1) => 3 ]) =>
-                                 [ [] '[ (f 1) => 3 ] ]
-  (separate-background-forms '[ (against-background (g) => 22)     (f 1) => 3 ]) =>
-                                    [ '[(g) => 22] '[ (f 1) => 3 ] ]
-  (separate-background-forms '[ (against-background (g) => 22)
-                    (f 1) => 3
-                    (against-background (h) => 3)]) => [ '[(g) => 22 (h) => 3]
-                                                         '[ (f 1) => 3 ] ])
 
+(tabular
+ (fact "separate-background-forms divides forms into background and other things"
+   (separate-background-forms '?in) => '[ ?background-forms  ?other-forms])
+
+ ?in                                    ?background-forms               ?other-forms
+ [ (against-background ..back1..
+                       ..back2..)
+   ..claim1..
+   ..claim2..]                         [..back1.. ..back2..]          [..claim1.. ..claim2..]
+
+ [ (against-background ..back1..)
+   ..claim1..
+   (against-background ..back2..)]      [..back1.. ..back2..]         [..claim1..] 
+
+ []                                     []                              []
+ [ (f 1) => 3 ]                         []                              [ (f 1) => 3 ]
+
+ [(against-background)
+  (f 1) => 3 ]                          []                              [(f 1) => 3]
+)
