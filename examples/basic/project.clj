9c826784c69942f6b7ec9083cc522c9de2ae0c8e
Brian Marick <marick@exampler.com>
2011-08-06 18:33:20 -0500

    announcing metaconstant dependencies

--- examples/basic/project.clj
+++ examples/basic/project.clj
@@ -3,6 +3,6 @@
   :dependencies [[org.clojure/clojure "[1.2.0,1.2.1]"]
                  [org.clojure/clojure-contrib "[1.2.0,1.2.1]"]
 		 ]
-  :dev-dependencies [[midje "1.2.0"]
+  :dev-dependencies [[midje "1.3-alpha1"]
                      [lein-midje "[1.0.0,)"]])
 
