bb89f8d0eaf5e9f2fd31aaf9b9b7c4f9ebc0fd65
Brian Marick <marick@exampler.com>
2011-10-11 12:36:00 -0500

    Removed direct dependency on monolithic contrib.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject midje "1.3-alpha2"
+(defproject midje "1.3-alpha3"
   :description "A TDD library for Clojure, with an emphasis on mocks"
   :dependencies [[org.clojure/clojure "[1.2.0,1.2.1]"]
-                 [org.clojure/clojure-contrib "1.2.0"]
                  [ordered "0.3.0"]
                  [org.clojure/math.combinatorics "0.0.1"]
                  [org.clojure/algo.monads "0.1.0"]
