3b3d744bab56723f95b222093fa11146cc7c62c8
Brian Marick <marick@exampler.com>
2011-10-18 22:29:07 -0500

    the :repository is no longer needed.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject midje "1.3-alpha4"
   :description "A TDD library for Clojure, with an emphasis on mocks"
-  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :dependencies [[org.clojure/clojure "[1.2.0],[1.2.1],[1.3.0]"]
+;  :dependencies [[org.clojure/clojure "[1.2.0],[1.2.1]"]
 ;                 [ordered "0.3.0"]  ; Not yet updated for Clojure 1.3.
                  [org.clojure/math.combinatorics "0.0.1"]
                  [org.clojure/algo.monads "0.1.0"]
