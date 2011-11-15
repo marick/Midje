ddee4b4ee7ced362d582dbdee7174e61ffc43d13
Alex Baranosky <alexander.baranosky@gmail.com>
2011-11-14 21:31:33 -0500

    added url and checksum-deps to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject midje "1.3-alpha5"
   :description "A TDD library for Clojure, with an emphasis on mocks"
+  :url "https://github.com/marick/Midje"
   :dependencies [[org.clojure/clojure "[1.2.0],[1.2.1],[1.3.0]"]
 ;  :dependencies [[org.clojure/clojure "[1.2.0],[1.2.1]"]
 ;                 [ordered "0.3.0"]  ; Not yet updated for Clojure 1.3.
@@ -8,5 +9,8 @@
                  [org.clojure/core.unify "0.5.1"]
                  [utilize "0.1.2"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
-                     [slamhound "1.1.1"]])
+                     [slamhound "1.1.1"]]
+  ;; automatically detect when your :dependencies key changes and run
+  ;; lein deps behind the scenes when necessary.
+  :checksum-deps true)
 
