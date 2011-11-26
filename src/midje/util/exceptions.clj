148ea37d593ab93095d5f6b57a996ad285b9057d
Alex Baranosky <alexander.baranosky@gmail.com>
2011-11-26 04:20:43 -0500

    adding some scoping

--- src/midje/util/exceptions.clj
+++ src/midje/util/exceptions.clj
@@ -6,25 +6,24 @@
 
 ;;; Writing
 
-(def line-separator (System/getProperty "line.separator"))
+(def line-separator ^{:private true} (System/getProperty "line.separator"))
 
 (defn user-error [& lines]
   (Error. (join line-separator lines)))
-
 
 ;;; Reading
 
 (defn stacktrace-as-strings [ex]
   (map #(.toString %) (.getStackTrace ex)))
 
-(defn remove-matches [re strings] 
-  (remove #(re-find re %) strings))
+(letfn [(remove-matches [re strings]
+          (remove #(re-find re %) strings))]
 
-(defn without-clojure-strings [all-strings]
-  (remove-matches #"^java\.|^clojure\.|^sun\.|^swank\.|^user\$eval" all-strings))
-  
-(defn without-midje-or-clojure-strings [all-strings]
-  (remove-matches #"^java\.|^clojure\.|^sun\.|^swank\.|^user\$eval|^midje" all-strings))
+  (defn without-clojure-strings [all-strings]
+    (remove-matches #"^java\.|^clojure\.|^sun\.|^swank\.|^user\$eval" all-strings))
+
+  (defn without-midje-or-clojure-strings [all-strings]
+    (remove-matches #"^java\.|^clojure\.|^sun\.|^swank\.|^user\$eval|^midje" all-strings)))
 
 (defn user-error-exception-lines [ex]
   (cons (.toString ex)
