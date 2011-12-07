15ff24bd831f778c629934a656d90b53c5fb6a59
Brian Marick <marick@exampler.com>
2011-12-07 12:28:32 -0600

    Keep swank-clojure out of lib/dev.

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "A TDD library for Clojure, with an emphasis on mocks"
   :url "https://github.com/marick/Midje"
   :dependencies [[org.clojure/clojure "[1.2.0],[1.2.1],[1.3.0]"]
-;  :dependencies [[org.clojure/clojure "[1.2.0],[1.2.1]"]
                  [ordered "1.0.0"]
                  [org.clojure/math.combinatorics "0.0.1"]
                  [org.clojure/algo.monads "0.1.0"]
@@ -10,9 +9,8 @@
                  [utilize "0.1.3"]
                  [colorize "0.1.1"]
                  [org.clojure/tools.macro "0.1.1"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [slamhound "1.1.1"]
-                     [com.intelie/lazytest "1.0.0-SNAPSHOT"]]
+  :dev-dependencies [[slamhound "1.1.1"]
+                     [com.intelie/lazytest "1.0.0-SNAPSHOT" :exclusions [swank-clojure]]]
   ;; automatically detects when your :dependencies key changes and runs
   ;; lein deps behind the scenes when necessary.
   :checksum-deps true)
