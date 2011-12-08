24a2addd5a7afa95e9a81356f7cab42d6f512f74
Brian Marick <marick@exampler.com>
2011-12-08 11:09:00 -0600

    Update to RC4

--- examples/compound-checkers/project.clj
+++ examples/compound-checkers/project.clj
@@ -3,5 +3,5 @@
   :dependencies
   [[org.clojure/clojure "[1.2.0,1.2.1]"]
    [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[midje "1.3.1-SNAPSHOT"]
+  :dev-dependencies [[midje "1.3.0-RC4"]
                      [lein-midje "[1.0.0,)"]])
