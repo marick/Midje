d42f6c4e80eb00bcfdcda223dd36b3cfea867932
Brian Marick <marick@exampler.com>
2011-12-12 10:04:40 -0600

    Bump to 1.3

--- examples/compound-checkers/project.clj
+++ examples/compound-checkers/project.clj
@@ -3,5 +3,5 @@
   :dependencies
   [[org.clojure/clojure "[1.2.0,1.2.1]"]
    [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[midje "1.3.0-RC4"]
+  :dev-dependencies [[midje "1.3.0"]
                      [lein-midje "[1.0.0,)"]])
