6b2b89cebdec41ed1354a79ac3b6805ade849460
Brian Marick <marick@exampler.com>
2011-12-19 19:00:40 -0600

    better test

--- test/behaviors/t_isolated_metaconstants.clj
+++ test/behaviors/t_isolated_metaconstants.clj
@@ -1,6 +1,7 @@
 ;; -*- indent-tabs-mode: nil -*-
 
 (ns behaviors.t-isolated-metaconstants
+  (:use [clojure.pprint])
   (:use [midje sweet]))
 
 
@@ -8,10 +9,10 @@
  true)
 
 (declare f g)
-;; (background (f) => 1 
-;;             (g) => 33)
+ (background (f) => 1 
+             (g) => 33)
 
-(midje.ideas.facts/fact2
+(fact
  1 => 1
  (provided
-   ..service.. =contains=> {:status 200}))
+   ..service.. =contains=> {:status 200}))))
