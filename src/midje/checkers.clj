4f2cfa857ef3a15ab36e3a5d5945c6cc027ac413
Alex Baranosky <alexander.baranosky@gmail.com>
2012-01-28 18:54:06 -0500

    republish and republish* are no longer are exposed to public API

--- src/midje/checkers.clj
+++ src/midje/checkers.clj
@@ -5,27 +5,23 @@
   midje.checkers
   (:use [midje.util.thread-safe-var-nesting :only [var-root]]))
 
-(defn republish* [namespace symbols]
-  (require namespace)
-  (doseq [sym symbols]
-    (let [^clojure.lang.Var var ( (ns-publics namespace) sym)]
-      (let [sym (with-meta sym (assoc (meta var) :ns *ns*))]
-        (if (.hasRoot var)
-          (intern *ns* sym (var-root var))
-          (intern *ns* sym))))))
+(letfn [(republish [namespace symbols]
+          (require namespace)
+          (doseq [sym symbols]
+            (let [^clojure.lang.Var var ((ns-publics namespace) sym)]
+              (let [sym (with-meta sym (assoc (meta var) :ns *ns*))]
+                (if (.hasRoot var)
+                  (intern *ns* sym (var-root var))
+                  (intern *ns* sym)))))) ]
 
-(defmacro republish [namespace & symbols]
-  `(republish* '~namespace '~symbols))
-
-(republish midje.checkers.defining
-           defchecker checker as-checker)
-(republish midje.checkers.chatty
-           chatty-checker)
-(republish midje.checkers.simple
-           truthy falsey TRUTHY FALSEY anything irrelevant exactly throws roughly)
-(republish midje.checkers.collection
-           has has-suffix has-prefix just contains
-           one-of two-of three-of four-of five-of six-of seven-of eight-of nine-of ten-of
-           n-of)
-(republish midje.checkers.deprecated
-           map-containing only-maps-containing maps-containing in-any-order)
+  (republish 'midje.checkers.defining
+             '[defchecker checker as-checker])
+  (republish 'midje.checkers.chatty
+             '[chatty-checker])
+  (republish 'midje.checkers.simple
+             '[truthy falsey TRUTHY FALSEY anything irrelevant exactly throws roughly])
+  (republish 'midje.checkers.collection
+             '[has has-suffix has-prefix just contains n-of
+              one-of two-of three-of four-of five-of six-of seven-of eight-of nine-of ten-of])
+  (republish 'midje.checkers.deprecated
+             '[map-containing only-maps-containing maps-containing in-any-order]))
