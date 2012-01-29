6ecf4a8b688498331dab87db3061b778369b82be
Alex Baranosky <alexander.baranosky@gmail.com>
2012-01-28 19:44:47 -0500

    added docstrings to all arrows

--- src/midje/ideas/arrow_symbols.clj
+++ src/midje/ideas/arrow_symbols.clj
@@ -1,13 +1,37 @@
 (ns ^{:doc "Lexicon of all Midje arrows, both expect arrows and prerequisite arrows."}
   midje.ideas.arrow-symbols)
 
-(def => "=>")
-(def =not=> "=not=>")
-(def =deny=> "=deny=>")
-(def =streams=> "=streams=>")
-(def =expands-to=> "=expands-to=>")
-(def =future=> "=future=>")
-(def =contains=> "=contains=>")
+(def ^{:doc "As a checking arrow: the left side should result in the right side.
+  As a prerequisite arrow: the fake should return the thing on the right."} 
+  => "=>")
+
+(def ^{:doc "Checking arrow. The left side should NOT result in the right side."} 
+  =not=> "=not=>")
+
+(def ^{:doc "Checking arrow. The left side should NOT result in the right side."} 
+  =deny=> "=deny=>")
+
+(def ^{:doc "Checking arrow. The left side should macroexpand-1 to the right side."} 
+  =expands-to=> "=expands-to=>")
+
+(def ^{:doc "Checking arrow. The check won't evaluate, but will 
+  leave a 'WORK TO DO' reminder in the report."} 
+  =future=> "=future=>")
+
+(def ^{:doc "Data prerequisite arrow.  
+  Ex. (fact 
+        (:a ..m..) => 1
+        (provided ..m.. =contains=> {:a 1}))"} 
+  =contains=> "=contains=>")
+
+(def ^{:doc "Prerequisite arrow. The fake will return the first element 
+  of the given sequence the first time the fake is evaluated, the second 
+  element the second time, and so on.
+
+  Ex. (def tally [] (+ (bar) (bar) (bar)))
+      (fact (tally) => 6
+        (provided (bar) =streams=> [1 2 3]))"} 
+  =streams=> "=streams=>")
 
 (def ^{:doc "Use this when testing Midje code that processes arrow-forms"}
      =test=> "=test=>")
