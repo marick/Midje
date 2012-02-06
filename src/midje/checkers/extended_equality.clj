2a91234a1d7901c3eff7ed1c578e948a561cddf5
Alex Baranosky <alexander.baranosky@gmail.com>
2012-02-05 22:46:51 -0500

    use core.match in extended-= to flatten nested conds/ifs

--- src/midje/checkers/extended_equality.clj
+++ src/midje/checkers/extended_equality.clj
@@ -2,7 +2,8 @@
 
 (ns ^{:doc "`=` extended for regular expressions, functions, etc."}
   midje.checkers.extended-equality
-  (:use [midje.checkers.chatty :only [chatty-checker-falsehood?]]
+  (:use [clojure.core.match :only [match]]
+        [midje.checkers.chatty :only [chatty-checker-falsehood?]]
         [midje.util.form-utils :only [classic-map? pairs record? regex?]]))
 
 (defn extended-fn? [x]
@@ -10,28 +11,21 @@
       (= (class x) clojure.lang.MultiFn)))
 
 (defn extended-= [actual expected]
-  (try
-    (cond (chatty-checker-falsehood? actual)
-          actual
-
-          (chatty-checker-falsehood? expected)
-          expected
-          
-          (extended-fn? expected)
-          (let [function-result (expected actual)]
-            (if (chatty-checker-falsehood? function-result) false function-result))
-        
-          (regex? expected)
-          (if (regex? actual)
-            (= (str actual) (str expected))
-            (re-find expected actual))
-
-          (and (record? actual) (classic-map? expected))
-          (= (into {} actual) expected)
-
-          :else
-          (= actual expected))
-    (catch Throwable ex false)))
+  (letfn [(evaluate-extended-fn [] 
+            (let [function-result (expected actual)]
+              (if (chatty-checker-falsehood? function-result) 
+                false 
+                function-result)))]
+    (try
+      (match [actual expected]
+        [(a :when chatty-checker-falsehood?) _]    actual
+        [_ (e :when chatty-checker-falsehood?)]    expected
+        [_ (e :when extended-fn?)]                 (evaluate-extended-fn) 
+        [(a :when regex?)  (e :when regex?)]       (= (str actual) (str expected))
+        [_                 (e :when regex?)]       (re-find expected actual)
+        [(a :when record?) (e :when classic-map?)] (= (into {} actual) expected)
+        [_ _]                                      (= actual expected))
+      (catch Throwable ex false))))
 
 (defn extended-list-=
   "Element-by-element comparison, using extended-= for the right-hand-side values."
