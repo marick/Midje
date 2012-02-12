478c1407990ef7e2090906a802411b5aaef6d750
Jacek Laskowski <jacek@japila.pl>
2012-02-12 21:14:45 +0100

    No deps duplication (inspired by http://corfield.org/blog/post.cfm/testing-your-project-against-multiple-versions-of-clojure)

--- project.clj
+++ project.clj
@@ -1,32 +1,26 @@
+(def common-deps '[[ordered "1.0.0"]
+                   [org.clojure/math.combinatorics "0.0.1"]
+                   [org.clojure/algo.monads "0.1.0"]
+                   [org.clojure/core.unify "0.5.1"]
+                   [utilize "0.2.2"]
+                   [colorize "0.1.1"]
+                   [org.clojure/tools.macro "0.1.1"]
+                   [org.clojure/core.incubator "0.1.0"]
+                   [org.clojure/core.match "0.2.0-alpha9"]])
+
 (defproject midje "1.3.2-alpha1"
   :description "A TDD library for Clojure that supports top-down ('mockish') TDD, encourages readable tests, provides a smooth migration path from clojure.test, balances abstraction and concreteness, and strives for graciousness."
   :url "https://github.com/marick/Midje"
-  :dependencies [[org.clojure/clojure "[1.3.0]"]
-                 [ordered "1.0.0"]
-                 [org.clojure/math.combinatorics "0.0.1"]
-                 [org.clojure/algo.monads "0.1.0"]
-                 [org.clojure/core.unify "0.5.1"]
-                 [utilize "0.2.2"]
-                 [colorize "0.1.1"]
-                 [org.clojure/tools.macro "0.1.1"]
-                 [org.clojure/core.incubator "0.1.0"]
-                 [org.clojure/core.match "0.2.0-alpha9"]]
+  :dependencies ~(conj common-deps
+                       '[org.clojure/clojure "1.3.0"])
 
-  ;; TODO: Alex Jan 29, 2012 - figure out how to not duplicate these deps 
+  ;; TODO: Alex Jan 29, 2012 - figure out how to not duplicate these deps
 
   :multi-deps {"1.2.0" [[org.clojure/clojure "1.2.0"]]
                "1.2.1" [[org.clojure/clojure "1.2.1"]]
                "1.3.0" [[org.clojure/clojure "1.3.0"]]
                "1.4.0" [[org.clojure/clojure "1.4.0-beta1"]]
-               :all [[ordered "1.0.0"]
-                     [org.clojure/math.combinatorics "0.0.1"]
-                     [org.clojure/algo.monads "0.1.0"]
-                     [org.clojure/core.unify "0.5.1"]
-                     [utilize "0.2.2"]
-                     [colorize "0.1.1"]
-                     [org.clojure/tools.macro "0.1.1"]
-                     [org.clojure/core.incubator "0.1.0"]
-                     [org.clojure/core.match "0.2.0-alpha9"]] }
+               :all common-deps }
   :dev-dependencies [[slamhound "1.2.0"]
                      [com.intelie/lazytest "1.0.0-SNAPSHOT" :exclusions [swank-clojure]]]
 
@@ -37,5 +31,3 @@
 
   ;; For Clojure snapshots
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
-
-
