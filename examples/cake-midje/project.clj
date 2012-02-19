9ce315fa7f54b4130104a7f4eb2c85d6465b6470
Brian Marick <marick@exampler.com>
2012-02-19 16:53:24 -0600

    The cake-midje example was more trouble than it's worth.

--- examples/cake-midje/project.clj
+++
@@ -1,5 +0,0 @@
-(defproject cake-midje "0.0.1-SNAPSHOT"
-  :description "TODO: add summary of your project"
-  :dependencies [[clojure "[1.2.0,1.2.1]"]
-		 [clojure-contrib "1.2.0"]]
-  :dev-dependencies [[midje "1.3.1-SNAPSHOT"]])
