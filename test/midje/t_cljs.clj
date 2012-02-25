a28d233d82af0da1a023b35bc9f679563955f4da
Robert Levy <r.p.levy@gmail.com>
2012-02-25 01:23:23 -0500

    unit test for cljs module

---
+++ test/midje/t_cljs.clj
@@ -0,0 +1,8 @@
+(ns midje.t-cljs
+  (:require [midje.cljs :as base])
+  (:use [midje.sweet :as sweet]))
+
+(fact
+ (do (base/load-file "midje/cljs/basic.cljs")
+     (base/cljs-eval '(doubler 4) 'midje.cljs.basic))
+ => 8)
