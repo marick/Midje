9a3b7c46508876ca3a98594d57569a266142854e
Brian Marick <marick@exampler.com>
2012-04-25 19:01:01 -0500

    Added extended-true?

--- src/midje/checkers/extended_falsehood.clj
+++ src/midje/checkers/extended_falsehood.clj
@@ -1,16 +1,19 @@
 (ns ^{:doc "Some failing checks carry additional information."}
   midje.checkers.extended-falsehood)
 
-(defn as-data-laden-falsehood [value] ; was as-chatty-falsehood 
+(defn as-data-laden-falsehood [value]
   (vary-meta value assoc :midje/data-laden-falsehood true))
 
-(defn data-laden-falsehood? [value]   ; chatty-checker-falsehood
+(defn data-laden-falsehood? [value]
   (:midje/data-laden-falsehood (meta value)))
 
 (defn data-laden-falsehood-to-map [value]
   (with-meta value {}))
 
-(defn extended-false? [value]    ; was chattily-false
+(defn extended-false? [value]
   (or (not value)
       (data-laden-falsehood? value)))
+
+(defn extended-true? [value]
+  (not (extended-false? value)))
 
--- test/midje/checkers/t_extended_falsehood.clj
+++ test/midje/checkers/t_extended_falsehood.clj
@@ -1,7 +1,6 @@
 (ns midje.checkers.t-extended-falsehood
   (:use midje.sweet
-        [midje.checkers.extended-falsehood :only [extended-false? as-data-laden-falsehood
-                                                  data-laden-falsehood?]]
+        [midje.checkers.extended-falsehood]
         midje.test-util
         clojure.pprint))
 
@@ -9,8 +8,19 @@
   (extended-false? false) => truthy
   (extended-false? true) => falsey
   (extended-false? {:intermediate-results 3}) => falsey
-  (extended-false? (as-data-laden-falsehood {})) => truthy)
+  (extended-false? (as-data-laden-falsehood {})) => truthy
+
+  ".. and its inverse"
+  (extended-true? false) => falsey
+  (extended-true? true) => truthy
+  (extended-true? {:intermediate-results 3}) => truthy
+  (extended-true? (as-data-laden-falsehood {})) => falsey)
 
 (facts "about data-laden falsehoods"
   (as-data-laden-falsehood [5]) => data-laden-falsehood?
   (meta (as-data-laden-falsehood (with-meta [5] {:foo true}))) => (contains {:foo true}))
+  
+  
+
+  
+  
