748a6f0b623c348ff7100ce7951d8720d59622f6
Brian Marick <marick@exampler.com>
2012-05-05 17:36:46 -0500

    Comment out "1.5.0" dependency set until there's something for it to download.

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
                "1.2.1" [[org.clojure/clojure "1.2.1"]]
                "1.3.0" [[org.clojure/clojure "1.3.0"]]
                "1.4.0" [[org.clojure/clojure "1.4.0"]]
-               "1.5.0" [[org.clojure/clojure "1.5.0-SNAPSHOT"]]
+;               "1.5.0" [[org.clojure/clojure "1.5.0-SNAPSHOT"]]
                :all ~common-deps }
   :dev-dependencies [[slamhound "1.2.0"]
                      [jonase/kibit "0.0.3"]
