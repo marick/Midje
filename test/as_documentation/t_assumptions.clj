5b4469d03dc1555489dc540c5f7e36a297175903
Brian Marick <marick@exampler.com>
2012-06-15 18:16:13 -0500

    Some thoughts about a replacement for background prerequisites.

--- test/as_documentation/t_assumptions.clj
+++ test/as_documentation/t_assumptions.clj
@@ -2,44 +2,115 @@
   (:use midje.sweet
         midje.test-util))
 
-(comment
 
-  (fact "text"
-    (examples-assume (alive? anything) => false)
-    ...cell... => dies?
-    (provided
-      (...)))
+;; (comment
 
 
-  (prerequisite-group [(all-status) => ?desired-status]
-    (status ...foo...) => ?desired-status
-    (status ...bar...) => ?desired-status
-    (status ...quux...) => ?desired-status)
+;; ;;; how to share prerequisites from other namespaces. Define a
+;; ;;; function and call it.
+
+;; (comment
+
+;;   (fact "something"
+;;     (assume-prerequisites (ping 1) => 200
+;;                           (ping "1") => 200
+;;                           (ping 'symbol) => 404)
+;;     (f 3) => 5
+;;     (provided ...))
+    
+;;   (prerequisite-group [(only-numberish-values-succeed?) => true]
+;;     (ping 1) => 200
+;;     (ping "1") => 200
+;;     (ping 'symbol) => 404)
+
+;;   (fact "text"
+;;     (assume-prerequisite (only-numberish-values-succeed?) => true)
+;;     ...)
+
   
-  (fact "text"
-    (examples-assume (all-status) => :dead)
 
-    ...cell... => dies?
-    )
-
-  ;; Empty the log before every example
-
-  (to-ensure [@log => empty?]
-    (reset! log []))
-
-(fact "something"
-  (prerequisites (f 1) => 3
-                 (f 2) => 4)
-  (ensure-prerequisite (table :whatever) => full?)
-  (g 3) => 5
-  (provided
-    (f 3) => 1))
-
-(fact
-  (allow-assumption-here (table :whim) => full))
-
-(allow-assumption-here (clean database) => true)
-
-)
+;;   (prerequisite-group [(numberish-value-status) => ?desired-status]
+;;     (ping 1) => ?desired-status
+;;     (ping "1") => ?desired-status
+;;     (ping 'symbol) => 404)
+  
+;;   (fact "text"
+;;     (assume-prerequisite (numberish-value-status) => 200)
+;;     ...)
 
 
+;;   (to-ensure [(log-contents) => ?checker]
+;;     (case ?checker
+;;       empty?        (reset! log [])
+;;       (contains 5)  (reset! log [1 2 5])))
+
+;;   (to-ensure [(log-contents) => ?checker]
+;;     (reset! log []))
+;;   (to-ensure [(log-contents) => (contains ?value)]
+;;     (reset! log (range 0 ?value)))
+
+
+;;   (fact
+;;     (ensure-prerequisite (horse-count) => 5
+;;                          (cow-count) => 5)
+;;     ...)
+
+;;   (to-ensure [(horse-count) => ?number]
+;;     (ensure-prerequisite (animal-count :species :horse) => ?number))
+
+
+;;   (to-ensure [(animal-count :species ?species) => ?number]
+;;     (dotimes [n ?number]
+;;       (create-animal :species ?species
+;;                      :name  (str "horse-" n))))
+    
+                         
+
+;;   (to-ensure [(horses
+  
+;;   (to-ensure [@log => empty?]
+;;     (reset! log []))
+
+;;   (fact 
+;;     (ensure-prerequisite @log => empty?)
+;;   ;; Empty the log before every example
+;;   )
+
+
+;;   (fact
+;;     (f 1) => 2  ; log in undefined state
+;;     (ensure-prerequisite @log => empty?)
+;;     (f 1) => 2)  ; log is forced to be empty
+;;     ...)
+
+
+
+;;   (fact
+;;     (ensure-prerequisite-here @log => empty?))
+
+
+;;   (fact "big claim"
+;;     (assume-prerequisite (outer 1) => 5)
+;;     (fact "subclaim"
+;;       (assume-prerequisite (inner 1) => 5)
+;;       ;; examples here see both the inner and outer prerequisites
+;;       )
+;;     ;; An example here would see only the outer prerequisite.
+;;     (fact "another subclaim")
+;;       (assume-prerequisite (inner 1) => "55555555555555555555")
+;;       ;; examples here see both prerequisites, but note that
+;;       ;; `(inner 1)` returns a different value. 
+;;       )
+    
+;; )
+
+;; (fact "you can insert into the database"
+;;   (ensure-prerequisite-here (table :table) => empty?)
+;;   (insert :table, :greeting "hi", :person "mom!") => truthy
+;;   (let [matches (select :table, :greeting "hi")
+;;         match (first matches)]
+;;     (count matches) => 1
+;;     (:greeting match) => "hi"
+;;     (:person match) => "mom!"))
+  
+;; )
