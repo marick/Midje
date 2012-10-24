64fdefe9b3d918f325339bc853fef323137f8096
Brian Marick <marick@exampler.com>
2012-10-24 15:53:07 -0500

    Correct `lein-midje` dependency

--- project.clj
+++ project.clj
@@ -25,7 +25,7 @@
                                   [jonase/eastwood "0.0.2"]
                                   [lein-multi "1.1.0"]
                                   [com.stuartsierra/lazytest "1.2.3"]]
-                   :plugins [[lein-midje "2.0.0-SNAPSHOT"]]}}
+                   :plugins [[lein-midje "2.0.0"]]}}
   :resource-paths ["test-resources"]
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/mit-license.php"
