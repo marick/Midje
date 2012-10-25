7aec9d48a401f8daf5bd4b978adcae0ab55a8aaa
Brian Marick <marick@exampler.com>
2012-10-25 14:40:10 -0500

    failure had to be adjusted because of fact metadata

--- test/midje/error_handling/t_exceptional_errors.clj
+++ test/midje/error_handling/t_exceptional_errors.clj
@@ -2,17 +2,17 @@
   (:use [midje sweet test-util]))
 
 (after-silently 
- (fact "description" =>)
+ (fact "description" (cons) =>)
  (fact @reported => (just (contains {:type :exceptional-user-error
                                      :description ["description"]
-                                     :macro-form '(fact "description" =>)}))))
+                                     :macro-form '(fact "description" (cons) =>)}))))
 
 ;; report ONLY top level fact's description when there's a error in nested facts
 
 (after-silently
   (fact "fine"
-    (fact "description" =>))
+    (fact "description" (cons) =>))
  (fact @reported => (just (contains {:type :exceptional-user-error
                                      :description ["fine"]
                                      :macro-form '(fact "fine"
-                                                    (fact "description" =>))}))))
+                                                    (fact "description" (cons) =>))}))))
