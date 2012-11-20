207ee439bdd5a78f20fe5e480cf38411cff0587c
Brian Marick <marick@exampler.com>
2012-11-20 17:00:30 -0600

    Added immigrate-from

--- src/midje/util/namespace.clj
+++ src/midje/util/namespace.clj
@@ -18,19 +18,30 @@
 (defn semi-sweet-keyword? [loc]
   (matches-symbols-in-semi-sweet-or-sweet-ns? '(expect fake not-called data-fake) loc))
 
-(defn immigrate
-  "Create a public var in this namespace for each public var in the
+(letfn [(move-var [var sym]
+          (let [sym (with-meta sym (assoc (meta var) :ns *ns*))]
+            (if (.hasRoot var)
+              (intern *ns* sym (var-root var))
+              (intern *ns* sym))))]
+  
+  (defn immigrate
+    "Create a public var in this namespace for each public var in the
   namespaces named by ns-names. The created vars have the same name, root
   binding, and metadata as the original except that their :ns metadata
   value is this namespace."
-  [& ns-names]
-  (doseq [ns ns-names]
-    (require ns)
-    (doseq [[sym ^clojure.lang.Var var] (ns-publics ns)]
-      (let [sym (with-meta sym (assoc (meta var) :ns *ns*))]
-        (if (.hasRoot var)
-          (intern *ns* sym (var-root var))
-          (intern *ns* sym))))))
+    [& ns-names]
+    (doseq [ns ns-names]
+      (require ns)
+      (doseq [[sym ^clojure.lang.Var var] (ns-publics ns)]
+        (move-var var sym))))
+
+  (defn immigrate-from
+    "Like `immigrate`, except wth a list of named symbols."
+    [ns symbols]
+    (doseq [sym symbols]
+      (move-var (ns-resolve ns sym) sym)))
+  )
+  
 
 (defn intern+keep-meta [ns sym v]
   (intern ns sym v)
