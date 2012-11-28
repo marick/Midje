f77e747805ceb590929b3ada04da2316f329816e
Brian Marick <marick@exampler.com>
2012-11-28 09:02:04 -0600

    Doc string fix.

--- src/midje/internal_ideas/fact_context.clj
+++ src/midje/internal_ideas/fact_context.clj
@@ -1,5 +1,4 @@
-(ns ^{:doc "A way to keep track of the pertinent context of the current nested fact.
-            Currently, that is only the fact's description/doc-string"}
+(ns ^{:doc "A way to keep track of the pertinent context of the current nested fact."}
   midje.internal-ideas.fact-context
   (:use [clojure.string :only [join]]))
 
