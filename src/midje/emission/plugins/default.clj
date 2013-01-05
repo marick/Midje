456293f85d1cca5aea9f7e5bffb5cda83492a7f1
Brian Marick <marick@exampler.com>
2013-01-04 18:30:59 -0600

    clean up requires

--- src/midje/emission/plugins/default.clj
+++ src/midje/emission/plugins/default.clj
@@ -1,7 +1,6 @@
 (ns ^{:doc "The default for Midje output"}
   midje.emission.plugins.default
-  (:require [midje.ideas.reporting.report :as report]
-            [midje.clojure-test-facade :as ctf]
+  (:require 
             [midje.ideas.reporting.levels :as levelly]
             [midje.emission.state :as state]
             [midje.emission.plugins.silence :as silence]))
