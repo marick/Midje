fbae3226daed6ae7eb8378950427bebc0fe477b6
Brian Marick <marick@exampler.com>
2013-01-09 16:16:31 -0600

    Rather silly auto-generation of counter-isolation macro

--- src/midje/emission/state.clj
+++ src/midje/emission/state.clj
@@ -9,26 +9,46 @@
 (def top-level-fact-output-lines [])
 (def fact-output-lines [])
 
+
+;;; At some point, figure out how to do nested splicing quotes.
+(defn make-defmacro-body [ns name reset-name set-name body]
+  `(let [original-value# (deref (deref (ns-resolve '~ns '~name)))]
+     (try
+       ((ns-resolve '~ns '~reset-name))
+       ~@body
+     (finally
+       ((ns-resolve '~ns '~set-name) original-value#)))))
+    
+
 (defmacro make-counter-atom [name & keys]
-  (letfn [(make-one-incrementer [key]
-            `(defn ~(symbol (str name ":inc" key "!")) []
-               (swap! ~name
-                      (partial merge-with +) {~key 1})))
-          (make-one-getter [key]
-            `(defn ~(symbol (str name key)) []
-               (~key (deref ~name))))]
-            
-    (let [fresh-name (symbol (str "fresh-" name))
-          reset-name (symbol (str "reset-" name "!"))]
+  (let [fresh-name (symbol (str "fresh-" name))
+        set-name (symbol (str "set-" name "!"))
+        reset-name (symbol (str "reset-" name "!"))
+        wrapper-name (symbol (str "with-isolated-" name))]
+    (letfn [(make-one-incrementer [key]
+              `(defn ~(symbol (str name ":inc" key "!")) []
+                 (swap! ~name
+                        (partial merge-with +) {~key 1})))
+            (make-one-getter [key]
+              `(defn ~(symbol (str name key)) []
+                 (~key (deref ~name))))
+            (make-defmacro []
+              (let [body-sym (gensym 'body-)]
+                `(defmacro ~wrapper-name [& ~body-sym]
+                   (make-defmacro-body '~(ns-name *ns*) '~name '~reset-name '~set-name ~body-sym))))]
       `(do
          (def ~name (atom 0))
          (ecosystem/when-1-3+ (.setDynamic (var ~name)))
          (def ~fresh-name ~(zipmap keys (repeat 0)))
+         (defn ~set-name [newval#] (swap! ~name (constantly newval#)))
          (defn ~reset-name [] (reset! ~name ~fresh-name))
          (~reset-name)
+         ~(make-defmacro)
          ~@(map make-one-getter keys) 
          ~@(map make-one-incrementer keys)))))
-       
+
+
+
 
 (make-counter-atom output-counters
   :midje-passes :midje-failures :clojure-test-passes :clojure-test-failures)
