f7b9be6a253e13aa1045d596019c10eac57315ed
Brian Marick <marick@exampler.com>
2013-01-17 18:19:09 -0600

    A parser for the kind of argument lists I'm liking.

--- src/midje/parsing/arglists.clj
+++ src/midje/parsing/arglists.clj
@@ -17,3 +17,42 @@
       [[           ]  default       non-levels])))
 
 
+(defn is-flag-segment-for? [flag-predicate]
+  (comp boolean flag-predicate first))
+
+(defn build-on-flag-keyword [original suffix]
+  (keyword (str (name original) suffix)))
+
+(defn make-segmentingizing-arglist-parser [& flag-descriptions]
+  (fn [arglist]
+    (let [all-arg-set (set (flatten flag-descriptions))
+          segments (partition-by all-arg-set arglist)
+          [[true-args] flag-segments] (split-with (complement (is-flag-segment-for? all-arg-set))
+                                                  segments)
+          [unmentioned-keys map-with-flag-data-added]
+          (loop [so-far {:true-args (vec true-args)}
+                 flag-selector (zipmap (map set flag-descriptions)
+                                       (map first flag-descriptions))
+                                       
+                 flag-segments flag-segments]
+            (let [[[flag] flag-args] (take 2 flag-segments)
+                  matching-selector (first (filter #(% flag) (keys flag-selector)))]
+              (cond (empty? flag-segments)
+                    [(vals flag-selector) so-far]
+                    
+                    matching-selector
+                    (recur (assoc so-far (build-on-flag-keyword (flag-selector matching-selector) "?") true
+                                  (build-on-flag-keyword (flag-selector matching-selector) "-args") (vec flag-args))
+                           (dissoc flag-selector matching-selector)
+                           (drop 2 flag-segments))
+                    
+                    :else
+                    (throw (Error. (str "It should be impossible for a flag-segment not to match a selector."
+                                        (pr-str flag-descriptions arglist)))))))]
+      (merge map-with-flag-data-added
+             (zipmap (map #(build-on-flag-keyword % "?") unmentioned-keys)
+                     (repeat false))))))
+
+               
+          
+          
--- test/midje/parsing/t_arglists.clj
+++ test/midje/parsing/t_arglists.clj
@@ -27,3 +27,47 @@
 
   (separate-print-levels [:print-nothing :print-facts] ..irrelevant..)
   => (throws Error #"extra.*print.*level"))
+
+
+
+(fact "Segmentizing arglist parser"
+  (let [flag-descriptions [[:dirs :dir] [:interval]]
+        parser (apply make-segmentingizing-arglist-parser flag-descriptions)]
+     
+    (fact "can find true args"
+    (parser []) => (contains {:true-args []})
+    (parser [:dirs]) => (contains {:true-args []}))
+      
+    (fact "can find flags that are present"
+      (parser ['arg :dirs 1 2]) => (contains '{:dirs? true :dirs-args [1 2]}))
+
+    (fact "can find flags that are absent"
+      (parser [:dirs 1 2]) => (contains '{:interval? false}))
+
+    (fact "flag synonyms are equivalent to the first argument in the description"
+      (parser [:dir 1 2]) => (contains '{:dirs? true :dirs-args [1 2]}))
+
+    (fact "putting it all together"
+      (parser []) => {:true-args []
+                      :dirs? false
+                      :interval? false}
+
+      (parser [1]) => {:true-args [1]
+                      :dirs? false
+                      :interval? false}
+
+      (parser [:dirs]) => {:true-args []
+                            :dirs? true
+                            :dirs-args []
+                            :interval? false}
+
+      (parser [:dir 1]) => {:true-args []
+                            :dirs? true
+                            :dirs-args [1]
+                            :interval? false}
+
+      (parser [1 :interval 2 3 :dir 4]) => {:true-args [1]
+                                            :dirs? true
+                                            :dirs-args [4]
+                                            :interval? true
+                                            :interval-args [2 3]})))
