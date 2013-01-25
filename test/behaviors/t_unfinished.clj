c0bc74bb54963063e95d39ad244cf8026aa0edfc
Brian Marick <marick@exampler.com>
2013-01-24 18:22:40 -0600

    Removed the internal-ideas directory

---
+++ test/behaviors/t_unfinished.clj
@@ -0,0 +1,13 @@
+(ns behaviors.t-unfinished
+  (:use [midje sweet test-util]))
+
+(unfinished backing-function)
+
+(fact "unfinished produces a function that throws an exception"
+  (backing-function 2) => (throws Error))
+
+(fact "it prints useful information about how the call was made"
+  (backing-function 2 "string") => (throws Error
+                                           #"no implementation"
+                                           #"\(backing-function 2 \"string\"\)"))
+  
