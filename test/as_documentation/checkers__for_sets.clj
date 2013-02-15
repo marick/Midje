17598d399950e851a9eafde6d17be71ef3319f4b
Brian Marick <marick@exampler.com>
2013-02-15 10:35:07 -0600

    Set documentation tests

---
+++ test/as_documentation/checkers__for_sets.clj
@@ -0,0 +1,21 @@
+(ns as-documentation.checkers--for-sets
+  (:use midje.sweet
+        midje.test-util))
+
+
+(fact "`just` provides extended equality to set equality"
+  #{3 8 1} => (just odd? 3 even?))
+
+(fact "`contains` works with subsets"
+  (fact "subsets of literal values"
+    #{1 2 3} => (contains 3))
+  (fact "subsets of checkers"
+    #{1 2 3} => (contains odd? even?)))
+
+
+(fact "checking properties of elements"
+  (fact "a known number"
+    #{1 3 5} => (three-of odd?))
+  (fact "number irrelevant"
+    #{1 3 5} => (has every? odd?)))
+
