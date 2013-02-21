31bfe97527398a8b8d28d479f5900c6c5ec0be29
josephwilk <joe@josephwilk.net>
2013-02-21 18:35:35 +0100

    Default to on for now for green tests.

--- src/midje/config.clj
+++ src/midje/config.clj
@@ -34,7 +34,7 @@
   *config* {:print-level :print-normally
             :visible-deprecation true
             :visible-future true
-            :visible-failure-namespaces false
+            :visible-failure-namespace true
             :partial-prerequisites false
             :check-after-creation true
             :emitter 'midje.emission.plugins.default
