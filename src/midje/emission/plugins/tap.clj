c692462dfe5c8e181ee8f1a8b940722a6692cb8d
giorgio-v <giorgio@refactor.it>
2013-03-02 10:11:18 +0100

    First cut of TAP reporter plugin
    The basic functionality is there. Some tests on Midje fail and future
    facts are not supported, yet.
    
    * src/midje/emission/plugins/tap.clj:

---
+++ src/midje/emission/plugins/tap.clj
@@ -0,0 +1,78 @@
+(ns ^{:doc "TAP output. See http://testanything.org"}
+    midje.emission.plugins.tap
+  (:use midje.emission.util)
+  (:require [midje.data.fact :as fact]
+            [midje.emission.state :as state]
+            [midje.emission.plugins.util :as util]
+            [midje.emission.plugins.silence :as silence]
+            [midje.emission.plugins.default-failure-lines :as lines]
+            [clojure.string :as str]))
+
+(def fact-counter (atom 0))
+
+(defn- format-failure-summary [failure-map]
+  (let [[_ _ & more] (lines/summarize failure-map)]
+    (map #(str "# " %) (conj more (apply str (:position failure-map))))))
+
+(defn fail [failure-map]
+  (do
+    (swap! fact-counter inc)
+    (util/emit-lines (format-failure-summary failure-map))
+    (util/emit-one-line
+     (str "not ok " @fact-counter))))
+
+(defn pass []
+  (do
+    (swap! fact-counter inc)
+    (util/emit-one-line (str "ok " @fact-counter))))
+
+(def last-namespace-shown (atom nil))
+
+(defn set-last-namespace-shown! [string]
+  (reset! last-namespace-shown string))
+
+(defn possible-new-namespace [namespace-symbol]
+  (when (not= namespace-symbol @last-namespace-shown)
+    (util/emit-one-line (str "# Namespace " namespace-symbol))
+    (set-last-namespace-shown! namespace-symbol)))
+
+(defn starting-fact-stream []
+  (set-last-namespace-shown! nil))
+
+(defn starting-to-check-fact [fact]
+  (util/emit-one-line (str "# Checking "
+                           (or (fact/name fact)
+                               (fact/description fact)
+                               (str "fact at " (midje-position-string
+                                                [(fact/file fact)
+                                                 (fact/line fact)]))))))
+
+
+(defn finishing-fact-stream [midje-counters clojure-test-map]
+  (let [passes-and-failures (+ (:midje-passes midje-counters) (:midje-failures midje-counters))]
+    (util/emit-one-line (str 1 ".." passes-and-failures
+                             (when-not passes-and-failures
+                               "# No facts were checked. Is that what you wanted?")))))
+
+(defn future-fact [description-list position]
+  (util/emit-one-line "")
+  (util/emit-one-line (str "WORK TO DO" " "
+                           (when-let [doc (util/format-nested-descriptions description-list)]
+                             (str (pr-str doc) " "))
+                           "at " (util/filename-lineno position))))
+
+(defn make-map [& keys]
+  (zipmap keys
+          (map #(ns-resolve *ns* (symbol (name %))) keys)))
+
+
+(def emission-map (merge silence/emission-map
+                         (make-map :fail
+                                   :pass
+                                   :future-fact
+                                   :starting-to-check-fact
+                                   :possible-new-namespace
+                                   :finishing-fact-stream
+                                   :starting-fact-stream)))
+
+(state/install-emission-map emission-map)
