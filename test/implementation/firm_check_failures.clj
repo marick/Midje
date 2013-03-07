6fbc369b59c14d56e56501aad54e640bf6b65a1c
Brian Marick <marick@exampler.com>
2013-03-07 16:29:10 -0600

    Add optional namespace to prerequisite count mismatch.

--- test/implementation/firm_check_failures.clj
+++ test/implementation/firm_check_failures.clj
@@ -6,8 +6,25 @@
 
 ; The namespace of the test should appear in test failure report
 
-(config/with-augmented-config {:visible-failure-namespace true}
-  (capturing-failure-output
-   (fact (+ 1 1) => 4)
-   (fact
-     @fact-output => #"implementation.firm-check-failures")))
+(unfinished g)
+(defn f [] 3)
+
+(facts "about namespaces in failure positions"
+  (config/with-augmented-config {:visible-failure-namespace true}
+    (capturing-failure-output
+     (fact
+       (f) => -3
+       (provided (g) => 88))
+     (fact
+       @fact-output => #".clj:17\s+implementation.firm-check-failures"
+       @fact-output => #".clj:16\s+implementation.firm-check-failures")))
+
+  (config/with-augmented-config {:visible-failure-namespace false}
+    (capturing-failure-output
+     (fact
+       (f) => -3
+       (provided (g) => 88))
+     (fact
+       @fact-output => #".clj:26"
+       @fact-output =not=> #"clj:26\s+implementation.firm-check-failures"
+       @fact-output => #".clj:25"))))
