b9dacad39edd63ff6d3647a09aea0771564ddad1
giorgio-v <giorgio@refactor.it>
2013-07-18 20:14:31 +0200

    Removed spurious test output
    * test/midje/emission/plugins/t_tap.clj:
    
    Signed-off-by: giorgio-v <giorgio@refactor.it>

--- test/midje/emission/plugins/t_tap.clj
+++ test/midje/emission/plugins/t_tap.clj
@@ -6,7 +6,7 @@
             [midje.emission.plugins.util :as util]))
 
 (defn innocuously [key & args]
-  (config/with-augmented-config {:emitter 'midje.emission.plugins.junit
+  (config/with-augmented-config {:emitter 'midje.emission.plugins.tap
                                  :print-level :print-facts}
     (captured-output (apply (key tap/emission-map) args))))
 
@@ -15,13 +15,17 @@
 
 (def test-failure-map
   {:type :some-prerequisites-were-called-the-wrong-number-of-times,
-   :namespace "midje.emission.plugins.t-junit"})
+   :namespace "midje.emission.plugins.t-tap"})
 
 (fact
  "starting a fact stream resets fact-counter and last-namespace-shown"
  (innocuously :starting-fact-stream) => ""
  @tap/fact-counter => 0
  @tap/last-namespace-shown => nil)
+
+(fact
+ "starting to check fact emits a message"
+ (innocuously :starting-to-check-fact test-fact) => "# Checking named\n")
 
 (fact
  "closing a fact stream generates the closing report"
@@ -43,7 +47,6 @@
                                         (tap/set-last-namespace-shown! nil)))]
   (fact
    "pass produces ok with the index of the test"
-   (tap/starting-to-check-fact test-fact)
    (innocuously :pass) => (contains "ok 1\n")
    (innocuously :pass) => (contains "ok 2\n")))
 
@@ -51,7 +54,6 @@
                                         (tap/set-last-namespace-shown! nil)))]
   (fact
    "failure produces not ok with the index of the test"
-   (tap/starting-to-check-fact test-fact)
    (innocuously :fail test-failure-map) => (contains "\nnot ok 1")
    (innocuously :fail test-failure-map) => (contains "\nnot ok 2")))
 
@@ -59,5 +61,4 @@
 (with-state-changes [(before :facts (do (reset! tap/fact-counter 0)
                                         (tap/set-last-namespace-shown! nil)))]
   (fact "failure also produces not ok"
-        (tap/starting-to-check-fact test-fact)
         (innocuously :fail test-failure-map) => (contains "\nnot ok 1")))
