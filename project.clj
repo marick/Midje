882cee5360398f6bbba2c4806fcad59c5f35b0b5
Brian Marick <marick@exampler.com>
2014-11-29 15:49:34 -0600

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
 (defproject midje "1.6.4-SNAPSHOT"
   :description "A TDD library for Clojure that supports top-down ('mockish') TDD, encourages readable tests, provides a smooth migration path from clojure.test, balances abstraction and concreteness, and strives for graciousness."
   :url "https://github.com/marick/Midje"
+  :pedantic? :warn
   :dependencies [[org.clojure/clojure "1.5.1"]
                  ;; upgrading to 1.3.2 produces record-type output for ordered maps
                  [ordered "1.2.0" :exclusions [org.clojure/clojure]]
-                 [org.clojure/math.combinatorics "0.0.7"]
+                 [org.clojure/math.combinatorics "0.0.8"]
                  ;; Changing following to 0.5.6 makes a t_unify test fail.
                  [org.clojure/core.unify "0.5.2" :exclusions [org.clojure/clojure]]
-                 [clj-time "0.7.0"]
+                 [clj-time "0.8.0"]
                  [utilize "0.2.3" :exclusions [org.clojure/clojure]]
                  [colorize "0.1.1" :exclusions [org.clojure/clojure]]
                  [org.clojure/tools.macro "0.1.5"]
@@ -15,17 +16,17 @@
                  [difform "1.1.2"]
                  [org.tcrawley/dynapath "0.2.3"]
                  [swiss-arrows "1.0.0"]
-                 [org.clojure/tools.namespace "0.2.4"]
-                 [slingshot "0.10.3"]
-                 [commons-codec/commons-codec "1.9"]
+                 [org.clojure/tools.namespace "0.2.7"]
+                 [slingshot "0.12.1"]
+                 [commons-codec/commons-codec "1.10"]
                  ;; upgrade to 0.6.5 causes a reducers-not-found error
                  [gui-diff "0.5.0"]]
   :profiles {:dev {:dependencies [[slamhound "1.5.5"]
                                   [jonase/kibit "0.0.8"]
-                                  [prismatic/plumbing "0.2.2"]
-                                  [jonase/eastwood "0.1.2"]]
-                   :plugins [[lein-midje "3.1.3"]]}
-             :test-libs {:dependencies [[prismatic/plumbing "0.2.1"]]}
+                                  [prismatic/plumbing "0.3.5"]
+                                  [jonase/eastwood "0.2.0"]]
+                   :plugins [[lein-midje "3.1.4-SNAPSHOT"]]}
+             :test-libs {:dependencies [[prismatic/plumbing "0.3.5"]]}
              :1.3 [:test-libs {:dependencies [[org.clojure/clojure "1.3.0"]]}]
              :1.4 [:test-libs {:dependencies [[org.clojure/clojure "1.4.0"]]}]
              :1.5.0 [:test-libs {:dependencies [[org.clojure/clojure "1.5.0"]]}]
