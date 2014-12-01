32e389e9056837c108ac1cf914889ec11156f510
Brian Marick <marick@exampler.com>
2014-11-30 18:20:27 -0600

    Further removal of 1.3 code

--- test/user/fus_threading.clj
+++ test/user/fus_threading.clj
@@ -2,33 +2,31 @@
   (:use midje.sweet
         midje.test-util
         midje.util.ecosystem))
-  
-(when-1-4+
 
- (use 'plumbing.core)
- (require '[plumbing.graph :as graph])
+(use 'plumbing.core)
+(require '[plumbing.graph :as graph])
 
- (defn tak-y [_] 5)
+(defn tak-y [_] 5)
 
- (defn tak [x y z]
-   (if (< y x)
-     (tak (tak (dec x) (tak-y 4) z)
-          (tak (dec y) (tak-y 4) x)
-          (tak (dec z) (tak-y 4) y))
-     z))
- 
- (def computation-tree 
-   {:one (fnk [arg] (tak arg (tak-y 4) 20))
-    :two (fnk [arg] (tak arg (tak-y 4) 20))
-    :three (fnk [arg] (tak arg (tak-y 4) 20))
-    :all (fnk [one two three] [one two three])})
- 
- (def computation (graph/par-compile computation-tree))
- 
- (fact "prerequisite bindings persist into other threads"
-   (:all (computation {:arg 20})) => [4 4 4]
-   (provided (tak-y 4) => 4))
- 
-) ; 1.4+
+(defn tak [x y z]
+  (if (< y x)
+    (tak (tak (dec x) (tak-y 4) z)
+         (tak (dec y) (tak-y 4) x)
+         (tak (dec z) (tak-y 4) y))
+    z))
 
-  
+(def computation-tree 
+  {:one (fnk [arg] (tak arg (tak-y 4) 20))
+   :two (fnk [arg] (tak arg (tak-y 4) 20))
+   :three (fnk [arg] (tak arg (tak-y 4) 20))
+   :all (fnk [one two three] [one two three])})
+
+(def computation (graph/par-compile computation-tree))
+
+(fact "prerequisite bindings persist into other threads"
+  (:all (computation {:arg 20})) => [4 4 4]
+  (provided (tak-y 4) => 4))
+
+
+
+
