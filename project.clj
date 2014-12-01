c6b2a19491afc0e39d9b7ba784297d3f65cd5a41
Brian Marick <marick@exampler.com>
2014-11-30 18:20:06 -0600

    Flare is only used in 1.5+

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
                  [org.tcrawley/dynapath "0.2.3"]
                  [swiss-arrows "1.0.0" :exclusions [org.clojure/clojure]]
                  [org.clojure/tools.namespace "0.2.7"]
-                 [flare "0.2.5"]
+                 [flare "0.2.5" :exclusions [org.clojure/clojure]]
                  [slingshot "0.12.1"]
                  [commons-codec/commons-codec "1.10"]
                  ;; upgrade to 0.6.5 causes a reducers-not-found error
--- src/midje/emission/plugins/flare.clj
+++ src/midje/emission/plugins/flare.clj
@@ -1,38 +1,43 @@
 (ns midje.emission.plugins.flare
-  (:require [flare.core :as flare]
+  (:use midje.clojure.core)
+  (:require [midje.util.ecosystem :as ecosystem]
             [midje.emission.plugins.util :as util]))
 
+(defn emit-flare-lines [& args])
 
-;; Following code adapted from
-;; https://raw.githubusercontent.com/andersfurseth/flare/master/src/flare/midje.clj
+(ecosystem/when-1-5+ 
+ (require '[flare.core :as flare])
+ ;; Following code adapted from
+ ;; https://raw.githubusercontent.com/andersfurseth/flare/master/src/flare/midje.clj
+ 
+ (defn report [reports]
+   (when (seq reports)
+     (util/emit-one-line "")
+     (doseq [report reports]
+       (util/emit-one-line report))))
 
-(defn report [reports]
-  (when (seq reports)
-    (util/emit-one-line "")
-    (doseq [report reports]
-      (util/emit-one-line report))))
+ (defn generate-report-for-keyed-diff [[path diffs]]
+   (let [diffs (map flare/report diffs)
+         indent-diffs? (and (seq path) (< 1 (count diffs)))]
+     (-> diffs
+         (cond->> indent-diffs? (map #(str "  " %)))
+         flare/join-with-newlines
+         (cond->> (seq path) (str "in " (pr-str path) (if indent-diffs? "\n" " "))))))
 
-(defn generate-report-for-keyed-diff [[path diffs]]
-  (let [diffs (map flare/report diffs)
-        indent-diffs? (and (seq path) (< 1 (count diffs)))]
-    (-> diffs
-        (cond->> indent-diffs? (map #(str "  " %)))
-        flare/join-with-newlines
-        (cond->> (seq path) (str "in " (pr-str path) (if indent-diffs? "\n" " "))))))
+ (defn generate-reports [diffs]
+   (prn "Flare data: " diffs)
+   (clojure.pprint/pprint diffs)
+   (try
+     (->> diffs
+          flare/flatten-keys
+          sort
+          (map flare/generate-report-for-keyed-diff))
+     (catch Exception e)))
 
-(defn generate-reports [diffs]
-  (prn "Flare data: " diffs)
-  (clojure.pprint/pprint diffs)
-  (try
-    (->> diffs
-         flare/flatten-keys
-         sort
-         (map flare/generate-report-for-keyed-diff))
-    (catch Exception e)))
-
-(defn emit-flare-lines [failure-map]
-  (when (= (:type failure-map) :actual-result-did-not-match-expected-value)
-    
-    (some-> (flare/diff (:expected-result failure-map) (:actual failure-map))
-            generate-reports
-            report)))
+ (defn emit-flare-lines [failure-map]
+   (when (= (:type failure-map) :actual-result-did-not-match-expected-value)
+     
+     (some-> (flare/diff (:expected-result failure-map) (:actual failure-map))
+             generate-reports
+             report)))
+)
