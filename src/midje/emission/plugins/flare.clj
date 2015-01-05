9825825c3cc06b1490b4f76c65674799f16a71f2
Anders Furseth <anders.furseth@gmail.com>
2015-01-05 11:16:50 +0100

    Migrate to flare 0.2.8

--- src/midje/emission/plugins/flare.clj
+++ src/midje/emission/plugins/flare.clj
@@ -5,40 +5,26 @@
 
 (defn emit-flare-lines [& args])
 
-(ecosystem/when-1-5+ 
+(ecosystem/when-1-5+
  (require '[flare.core :as flare])
  ;; Following code adapted from
  ;; https://raw.githubusercontent.com/andersfurseth/flare/master/src/flare/midje.clj
- 
+
  (defn report [reports]
    (when (seq reports)
      (util/emit-one-line "")
      (doseq [report reports]
        (util/emit-one-line report))))
 
- (defn generate-report-for-keyed-diff [[path diffs]]
-   (let [diffs (map flare/report diffs)
-         indent-diffs? (and (seq path) (< 1 (count diffs)))]
-     (-> diffs
-         (cond->> indent-diffs? (map #(str "  " %)))
-         flare/join-with-newlines
-         (cond->> (seq path) (str "in " (pr-str path) (if indent-diffs? "\n" " "))))))
-
  (defn generate-reports [diffs]
    ; (prn "Flare data: " diffs)
    ; (clojure.pprint/pprint diffs)
    (println "Flare output (experimental):")
-   (try
-     (->> diffs
-          flare/flatten-keys
-          sort
-          (map flare/generate-report-for-keyed-diff))
-     (catch Exception e)))
+   (flare/generate-reports diffs))
 
  (defn emit-flare-lines [failure-map]
    (when (= (:type failure-map) :actual-result-did-not-match-expected-value)
-     
-     (some-> (flare/diff (:expected-result failure-map) (:actual failure-map))
-             generate-reports
-             report)))
+      (some-> (flare/diff (:expected-result failure-map) (:actual failure-map))
+              generate-reports
+              report)))
 )
