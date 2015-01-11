7dfa7ac9ca0f5eff4ffec9de696e01ff2e63cb3b
Brian Marick <marick@exampler.com>
2015-01-10 20:54:12 -0600

    Begin beefing up `classify` for collection-diffs

--- src/midje/checking/checkers/collection_diffs.clj
+++ src/midje/checking/checkers/collection_diffs.clj
@@ -7,16 +7,28 @@
             [midje.checking.checkers.collection :as old]
             [midje.checking.core :as core]
             flare.map)
-  (:import [flare.map MapKeysDiff]))
+  (:import [flare.map MapKeysDiff]
+           [flare.atom AtomDiff]))
 
 ;; TODO: Currently not handling "looseness"
 
-(defn classify [actual expected] :map:map)
+(defn classify [actual expected looseness]
+  (cond (and (map? actual) (map? expected))  :map:map
+        :else :unknown))
 
-(defmulti contains:diffs (fn [actual expected & _] (classify actual expected)))
+(defmulti contains:diffs (fn [actual expected looseness] (classify actual expected looseness)))
 
-(defmethod contains:diffs :map:map [actual expected]
-  (let [missing-keys (difference (set (keys expected)) (set (keys actual)))]
-    (if (empty? missing-keys)
-      []
-      [(new MapKeysDiff missing-keys nil)])))
+(defmethod contains:diffs :map:map [actual-map expected-map _]
+  (let [[actual-keys expected-keys] (map #(set (keys %)) [actual-map expected-map])
+        missing-keys (difference expected-keys actual-keys)
+        
+        diff-map (reduce (fn [so-far shared-key] 
+                           (let [[actual-val expected-val] (map shared-key [actual-map expected-map])]
+                             (if (not= actual-val expected-val)
+                               (assoc so-far shared-key (new AtomDiff actual-val expected-val))
+                               so-far)))
+                          {}
+                          (difference expected-keys missing-keys))]
+    (-> []
+        (cond-> (not-empty? missing-keys) (conj (new MapKeysDiff missing-keys nil))
+                (not-empty? diff-map) (conj diff-map)))))
--- test/implementation/checking/checkers/fim_collection_diffs.clj
+++ test/implementation/checking/checkers/fim_collection_diffs.clj
@@ -1,33 +1,69 @@
 (ns implementation.checking.checkers.fim-collection-diffs (:require [midje.checking.checkers.collection-diffs :as subject])
   (:use midje.sweet midje.test-util)
   (:require [midje.checking.core :as core])
-  (:import [flare.map MapKeysDiff]))
+  (:import [flare.map MapKeysDiff]
+           [flare.atom AtomDiff]))
+
+(defrecord R [])
+
+(fact classifications
+  (subject/classify {} {} #{}) => :map:map
+  (subject/classify {} {} #{:gaps-ok}) => :map:map
+  (subject/classify {} {} #{:in-any-order}) => :map:map
+
+  (subject/classify {} (R.) #{}) => :map:map
+  (subject/classify (R.) {} #{:gaps-ok :in-any-order}) => :map:map
+  (subject/classify (R.) (R.) #{}) => :map:map
+  
+  (subject/classify :other :things #{}) => :unknown)
+
+
+
             
 
 ;; Eventually, the `differ` will be the actual checker, making this format a bit more
 ;; readable.
-(defn c [actual differ expected]
-  (differ actual expected))
-  ;; (let [result ((checker expected) actual)]
-  ;;   (if (core/data-laden-falsehood? result)
-  ;;     (:diffs (core/data-laden-falsehood-to-map result))
-  ;;     result)))
-  
-(fact "success cases"
-  (c {:a 1} subject/contains:diffs {:a 1}) => empty?
-  (c {:a 1} subject/contains:diffs {}) => empty?
-  (c {:a 1 :b 1} subject/contains:diffs {:a 1}) => empty?)
+(defn result-of
+  ([actual differ expected looseness] (differ actual expected looseness))
+  ([actual differ expected] (result-of actual differ expected #{})))
 
+(defrecord TwoArgs [l1 l2])
 
-(fact "missing keys"
-  (c {} subject/contains:diffs {:a 1}) => [(new MapKeysDiff #{:a} nil)]
-  (c {:a 1} subject/contains:diffs {:a 1 :b 2 :c 3}) => [(new MapKeysDiff #{:b :c} nil)])
+(fact "maplike things"
+  (fact "success cases"
+    (result-of {:l1 1} subject/contains:diffs {:l1 1}) => empty?
+    (result-of {:l1 1} subject/contains:diffs {}) => empty?
+    (result-of {:l1 1 :l2 1} subject/contains:diffs {:l1 1}) => empty?)
 
-(future-fact "compares values using extended equality"
-  (c {:a "123"} subject/contains:diffs {:a #"\d\d\d"}) => 1 ; [(new ExpectedRegexpDiff #"\d\d\d")]
-  (c {:a 1} subject/contains:diffs {:a odd?}) => 1; [(new MapKeysDiff #{:a})])
+  (fact "missing keys"
+    (result-of {} subject/contains:diffs {:l1 1}) => [(new MapKeysDiff #{:l1} nil)]
+    (result-of {:l1 1} subject/contains:diffs {:l1 1 :l2 2 :l3 3}) => [(new MapKeysDiff #{:l2 :l3} nil)])
+
+  (fact "incorrect values"
+    (result-of {:l1 1} subject/contains:diffs {:l1 2}) => [{:l1 (new AtomDiff 1 2)}]
+    (result-of {:l1 1, :l2 :irrelevant} subject/contains:diffs {:l1 2}) => [{:l1 (new AtomDiff 1 2)}]
+
+    ;; does not try to compare a missing key
+    (result-of {:l1 1, :l2 2} subject/contains:diffs {:l1 11, :l2 22 :l3 3})
+    => (just (new MapKeysDiff #{:l3} nil)
+             {:l1 (new AtomDiff 1 11) :l2 (new AtomDiff 2 22)}
+             :in-any-order))
+
+  (fact "Records are processed as maps"
+    (result-of (new TwoArgs 1 2) subject/contains:diffs {:l1 1}) => empty?
+    (result-of (new TwoArgs 1 2) subject/contains:diffs {:l1 2}) => [{:l1 (new AtomDiff 1 2)}]
+    (result-of (new TwoArgs 1 2) subject/contains:diffs {:l1 2 :l2 2 :l3 3})
+    => (just {:l1 (new AtomDiff 1 2)}
+             (new MapKeysDiff #{:l3} nil)
+             :in-any-order)
+
+    (result-of (new TwoArgs 1 2) subject/contains:diffs (new TwoArgs 1 2)) => empty?
+    (result-of (new TwoArgs 1 2) subject/contains:diffs (new TwoArgs 2 2)) => [{:l1 (new AtomDiff 1 2)}])
+
+  (future-fact "compares values using extended equality"
+    (result-of {:l1 "123"} subject/contains:diffs {:l1 #"\d\d\d"}) => 1 ; [(new Result-OfRegexpDiff #"\d\d\d")]
+    (result-of {:l1 1} subject/contains:diffs {:a odd?}) => 1; [(new MapKeysDiff #{:a})])
+    )
+
+  (future-fact "Note that extended-equality matches aren't erroneously included in the output")
 )
-
-
-
-
