0e039191a71acab0eaa1c32bb205ae60b4147238
Brian Marick <marick@exampler.com>
2015-01-10 21:32:12 -0600

    chopping to minimal 1.7-alpha5 issue

--- test/midje/util/t_one_seven.clj
+++ test/midje/util/t_one_seven.clj
@@ -3,23 +3,6 @@
         [midje.util laziness thread-safe-var-nesting]
         [midje.test-util]))
 
-;; Justification for use of eagerly
-(def counter (atom :needs-to-be-initialized))
-(def #^:dynamic *mocked-function-produces-next-element* inc)
-
-(defn function-under-test-produces-a-lazy-list []
-  (iterate *mocked-function-produces-next-element* 1))
-
-(defn mock-use []
-  (binding [*mocked-function-produces-next-element* (fn [n] (swap! counter inc) (inc n))]
-    (eagerly (take 5 (function-under-test-produces-a-lazy-list)))))
-
-(fact "eagerly forces evaluation"
-  (reset! counter 1)
-  (mock-use)
-  @counter => 5)
-
-;; After justification, more facts.
 
 (unfinished exploder)
 
