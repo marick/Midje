8e34b919b90f58748c11a90ef4aa3de00c5786b2
Brian Marick <marick@exampler.com>
2015-04-20 16:39:46 -0500

    Moved functions into `pile`

--- src/midje/util/pile.clj
+++ src/midje/util/pile.clj
@@ -1,7 +1,8 @@
-(ns ^{:doc "Functions that are somewhat general purpose, but seem too obscure to be in midje.clojure.core.
+(ns ^{:doc "Functions that are somewhat general purpose, but seem too obscure to be in marick.clojure.core.
            A cue to wiser placement decisions someday."}
   midje.util.pile
-  (:use midje.clojure.core)
+  (:use marick.clojure.core)
+  (:use [ordered.map :only [ordered-map]])
   (:import org.apache.commons.codec.digest.DigestUtils))
 
 ;;; Named things
@@ -31,7 +32,35 @@
 (defn sort-map [m]
   (into (sorted-map) m))
 
+;;; Copied from utilize to remove dependencies. 
+;;; https://github.com/AlexBaranosky/Utilize
+(defn ordered-zipmap [keys vals]
+  "Like zipmap, but guarantees order of the entries"
+  (loop [m (ordered-map)
+         ks (seq keys)
+         vs (seq vals)]
+    (if (and ks vs)
+      (recur (assoc m (first ks) (first vs)) 
+             (next ks)
+             (next vs))
+      m)))
+
+
+
 ;;; Sequences
+
+(defn rotations
+  "Returns a lazy seq of all rotations of a seq"
+  [coll]
+  (for [i (range 0 (count coll))]
+    (lazy-cat (drop i coll) (take i coll))))
+
+(defn map-first
+  "Like map, but applies f to only the first element of the seq"
+  [f x]
+  (cons (f (first x)) (rest x)))
+
+
 
 (defn apply-pairwise
   "(apply-pairwise [inc dec] [1 1] [2 2]) => [ [2 0] [3 1] ]
@@ -58,6 +87,26 @@
   (partial pop-if map?))
 
 
+
+;;; Namespaces
+
+(defn alias-var  ;; from `useful`
+  "Create a var with the supplied name in the current namespace, having the same
+metadata and root-binding as the supplied var."
+  [name ^clojure.lang.Var var]
+  (apply intern *ns*
+         (with-meta name (merge {:dont-test (str "Alias of " (var-name var))}
+                                (meta var)
+                                (meta name)))
+         (when (.hasRoot var) [@var])))
+
+
+(defmacro defalias  ;; from `useful`
+  "Defines an alias for a var: a new var with the same root binding (if
+any) and similar metadata. The metadata of the alias is its initial
+metadata (as provided by def) merged into the metadata of the original."
+  [dst src]
+  `(alias-var (quote ~dst) (var ~src)))
 
 ;;; Higher-order predicate helpers
 
--- test/midje/util/t_pile.clj
+++ test/midje/util/t_pile.clj
@@ -1,40 +1,43 @@
 (ns midje.util.t-pile
-  (:use [midje.util.pile])
   (:use [midje.sweet])
-  (:use [midje.test-util]))
+  (:use [midje.test-util])
+  (:require [midje.util.pile :as subject]))
 
 (facts "you can tack new keys onto a hashmap"
-  (tack-on-to {:a [1], :b [55] :c 'blah} :a 2 :b 56) => {:a [1 2], :b [55 56], :c 'blah})
+  (subject/tack-on-to {:a [1], :b [55] :c 'blah} :a 2 :b 56) => {:a [1 2], :b [55 56], :c 'blah})
 
 (fact "map-difference"
-  (map-difference {:a 1, :b 2} {:a 1, :c 3}) => {:b 2})
+  (subject/map-difference {:a 1, :b 2} {:a 1, :c 3}) => {:b 2})
 
   
 (fact "apply each function to each corresponding arg" 
-  (apply-pairwise [inc dec] [1 1] [2 2]) => [[2 0] [3 1]])
+  (subject/apply-pairwise [inc dec] [1 1] [2 2]) => [[2 0] [3 1]])
 
+(fact 
+  (subject/map-first str [1 2 3]) => ["1" 2 3])
 
 (fact "sort a map"
-  (sort-map {:z 26 :b 2 :a 1}) => {:a 1 :b 2 :z 26})
+  (subject/sort-map {:z 26 :b 2 :a 1}) => {:a 1 :b 2 :z 26})
+
 
 
 (fact "any-pred-from"
-  ((any-pred-from [odd? even?]) 1) => true
-  ((any-pred-from [pos? neg?]) 0) => false
-  ((any-pred-from [:key :word]) {:key false}) => false
-  ((any-pred-from [:key :word]) {:key false :word 3}) => true
-  ((any-pred-from [#{1 2} #{3 4}]) 3) => true
+  ((subject/any-pred-from [odd? even?]) 1) => true
+  ((subject/any-pred-from [pos? neg?]) 0) => false
+  ((subject/any-pred-from [:key :word]) {:key false}) => false
+  ((subject/any-pred-from [:key :word]) {:key false :word 3}) => true
+  ((subject/any-pred-from [#{1 2} #{3 4}]) 3) => true
   ;; stops at first match
-  ((any-pred-from [(partial = 3) (fn[_](throw (new Error "boom!")))]) 3) => true
+  ((subject/any-pred-from [(partial = 3) (fn[_](throw (new Error "boom!")))]) 3) => true
   ;; Any empty list means that everything matches
-  ((any-pred-from []) 3) => true)
+  ((subject/any-pred-from []) 3) => true)
 
 (fact "stringlike-matches?"
-  (stringlike-matches? "foo" "ofoop") => true
-  (stringlike-matches? "foo" "ooop") => false
-  (stringlike-matches? "foo" nil) => false
-  (stringlike-matches? "foo" [1 2 3]) => false
-  (stringlike-matches? #"fo." "ofop") => true
-  (stringlike-matches? #"fo." "ooop") => false
-  (stringlike-matches? #"fo." false) => false)
+  (subject/stringlike-matches? "foo" "ofoop") => true
+  (subject/stringlike-matches? "foo" "ooop") => false
+  (subject/stringlike-matches? "foo" nil) => false
+  (subject/stringlike-matches? "foo" [1 2 3]) => false
+  (subject/stringlike-matches? #"fo." "ofop") => true
+  (subject/stringlike-matches? #"fo." "ooop") => false
+  (subject/stringlike-matches? #"fo." false) => false)
 
