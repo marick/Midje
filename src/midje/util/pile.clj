78160803011fc02fc89cb3e9de7dde2294c52a7a
Brian Marick <marick@exampler.com>
2015-04-20 17:00:25 -0500

    remove defalias

--- src/midje/util/pile.clj
+++ src/midje/util/pile.clj
@@ -86,28 +86,6 @@
   ;; "Extracts optional docstring from head of args"
   (partial pop-if map?))
 
-
-
-;;; Namespaces
-
-(defn alias-var  ;; from `useful`
-  "Create a var with the supplied name in the current namespace, having the same
-metadata and root-binding as the supplied var."
-  [name ^clojure.lang.Var var]
-  (apply intern *ns*
-         (with-meta name (merge {:dont-test (str "Alias of " (var-name var))}
-                                (meta var)
-                                (meta name)))
-         (when (.hasRoot var) [@var])))
-
-
-(defmacro defalias  ;; from `useful`
-  "Defines an alias for a var: a new var with the same root binding (if
-any) and similar metadata. The metadata of the alias is its initial
-metadata (as provided by def) merged into the metadata of the original."
-  [dst src]
-  `(alias-var (quote ~dst) (var ~src)))
-
 ;;; Higher-order predicate helpers
 
 (defn any-pred-from
