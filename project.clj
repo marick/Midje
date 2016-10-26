5c80966287707f7d3f92ab417843698cbbca59be
Brian Marick <marick@exampler.com>
2016-10-26 16:34:59 -0500

    update compatibility test

--- project.clj
+++ project.clj
@@ -20,10 +20,9 @@
   :profiles {:dev {:dependencies [[prismatic/plumbing "0.5.3"]]
                    :plugins [[lein-midje "3.2.1"]]}
              :test-libs {:dependencies [[prismatic/plumbing "0.5.3"]]}
-             :1.6 [:test-libs {:dependencies [[org.clojure/clojure "1.6.0"]]}]
              :1.7 [:test-libs {:dependencies [[org.clojure/clojure "1.7.0"]]}]
              :1.8 [:test-libs {:dependencies [[org.clojure/clojure "1.8.0"]]}]
-             :1.9 [:test-libs {:dependencies [[org.clojure/clojure "1.9.0-alpha11"]]}]
+             :1.9 [:test-libs {:dependencies [[org.clojure/clojure "1.9.0-alpha13"]]}]
              ;; The following profile can be used to check that `lein with-profile`
              ;; profiles are obeyed. Note that profile `:test-paths` *add on* to the
              ;; defaults.
@@ -37,8 +36,8 @@
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
 
-  :aliases {"compatibility" ["with-profile" "1.6:1.7:1.8:1.9" "midje" ":config" ".compatibility-test-config"]
-            "travis" ["with-profile" "1.6:1.7:1.8:1.9" "midje"]}
+  :aliases {"compatibility" ["with-profile" "1.7:1.8:1.9" "midje" ":config" ".compatibility-test-config"]
+            "travis" ["with-profile" "1.7:1.8:1.9" "midje"]}
 
   ;; For Clojure snapshots
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"
