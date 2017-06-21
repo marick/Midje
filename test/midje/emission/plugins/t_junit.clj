854a1b6cc190b4f1f0f8c58977648b4bc4b19152
Phillip Mates <phillip.mates@nubank.com.br>
2017-06-21 12:06:12 -0300

    Reset namespace after tests finish

--- test/midje/emission/plugins/t_junit.clj
+++ test/midje/emission/plugins/t_junit.clj
@@ -23,7 +23,8 @@
       (prerequisites
        (#'plugin/log-fn) => #(println %)
        (#'plugin/clear-file (contains "test-namespace")) => nil :times 1
-       (#'plugin/clear-file (contains "other-namespace")) => nil :times 1)
+       (#'plugin/clear-file (contains "other-namespace")) => nil :times 1
+       (#'plugin/clear-file (contains "placeholder-to-reset-namespace")) => nil :times 1)
 
       (innocuously :possible-new-namespace 'test-namespace)
       => (contains "<testsuite name='test-namespace'")
@@ -33,7 +34,8 @@
       ;; entering a different namespace must close previous one
       (innocuously :possible-new-namespace 'other-namespace)
       ;; FIXME: nicer test for 'both strings present'
-      => (contains (str "</testsuite>" "\n" "<testsuite name='other-namespace'")))
+      => (contains (str "</testsuite>" "\n" "<testsuite name='other-namespace'"))
+      (innocuously :possible-new-namespace 'placeholder-to-reset-namespace))
 
 ;; FIXME: this statefullnes is ugly and hard to test.
 ;; if this fact runs standalone, the :possible-new-namespace will try to clear
@@ -42,10 +44,12 @@
 (fact "Closing a fact stream closes testsuite"
       (prerequisites
        (#'plugin/clear-file (contains "test-namespace")) => nil
+       (#'plugin/clear-file (contains "placeholder-to-reset-namespace")) => nil :times 1
        (#'plugin/log-fn) => #(println %))
       (innocuously :possible-new-namespace 'test-namespace)
       (innocuously :finishing-fact-stream {} {})
-      => (contains "</testsuite>"))
+      => (contains "</testsuite>")
+      (innocuously :possible-new-namespace 'placeholder-to-reset-namespace))
 
 (fact "pass produces a <testcase> tag"
   (plugin/starting-to-check-fact test-fact)
