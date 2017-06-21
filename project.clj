60b8c1a696efa6dec04da2f5fbfd6aabd613a6fb
Phillip Mates <phillip.mates@nubank.com.br>
2017-06-21 16:13:13 -0300

    bump to clojure 1.9.0-alpha17

--- project.clj
+++ project.clj
@@ -22,7 +22,7 @@
              :test-libs {:dependencies [[prismatic/plumbing "0.5.4"]]}
              :1.7 [:test-libs {:dependencies [[org.clojure/clojure "1.7.0"]]}]
              :1.8 [:test-libs {:dependencies [[org.clojure/clojure "1.8.0"]]}]
-             :1.9 [:test-libs {:dependencies [[org.clojure/clojure "1.9.0-alpha13"]]}]
+             :1.9 [:test-libs {:dependencies [[org.clojure/clojure "1.9.0-alpha17"]]}]
              ;; The following profile can be used to check that `lein with-profile`
              ;; profiles are obeyed. Note that profile `:test-paths` *add on* to the
              ;; defaults.
