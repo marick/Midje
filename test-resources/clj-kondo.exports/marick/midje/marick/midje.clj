0a7495323d7189dc43399fe129166c27b9cc185f
Eric Dallo <ericdallo06@hotmail.com>
2021-05-10 18:11:55 -0300

    Improve clj-kondo support for tabular

--- test-resources/clj-kondo.exports/marick/midje/marick/midje.clj
+++ test-resources/clj-kondo.exports/marick/midje/marick/midje.clj
@@ -2,13 +2,23 @@
   (:require [clj-kondo.hooks-api :as hooks]
             [clojure.string :as string]))
 
-(defn table-variable? [node]
-  (let [sexpr (hooks/sexpr node)]
-    (and (symbol? sexpr)
-         (string/starts-with? (str sexpr) "?"))))
+(def arrows '#{=>
+               =not=>
+               =deny=>
+               =expands-to=>
+               =future=>
+               =contains=>
+               =streams=>
+               =throws=>
+               =test=>
+               =throw-parse-exception=>})
 
-(defn ^:private let-with-table-variables [body bindings]
-  (let [new-bindings (vec (reduce (fn [acc i] (concat acc [i (hooks/token-node 'identity)]))
+(defn ^:private index-of [e coll]
+  (first (keep-indexed #(when (= e %2) %1) coll)))
+
+(defn ^:private let-form [body bindings]
+  (let [new-bindings (vec (reduce (fn [acc i]
+                                    (concat acc [i (hooks/token-node 'identity)]))
                                   [] bindings))]
     (hooks/list-node
      [(hooks/token-node 'let)
@@ -17,15 +27,50 @@
 
 (defn ^:private do-form [forms]
   (hooks/list-node
-    (concat [(hooks/token-node 'do)]
-            forms)))
+   (concat [(hooks/token-node 'do)]
+           forms)))
 
-(defn tabular [{:keys [node]}]
-  (let [[facts vec-bindings & bindings] (rest (:children node))]
+(defn ^:private table-variable? [node]
+  (let [sexpr (hooks/sexpr node)]
+    (and (symbol? sexpr)
+         (string/starts-with? (str sexpr) "?"))))
+
+(defn ^:private handle-fact-outside-tabular [children arrow]
+  (let [pos-arrow-index (inc (index-of arrow children))
+        body (do-form children)
+        bindings (->> children
+                      (map-indexed (fn [i el]
+                                     (when (> i pos-arrow-index)
+                                       el)))
+                      (remove nil?))]
+    (if (hooks/vector-node? (first bindings))
+      {:node (->> (hooks/sexpr (first bindings))
+                  (map hooks/token-node)
+                  (let-form body))}
+      {:node (->> bindings
+                  (filter table-variable?)
+                  (let-form body))})))
+
+(defn fact-tabular [fact vec-bindings bindings]
+  (let [body (do-form (cons fact bindings))]
     (if (hooks/vector-node? vec-bindings)
-      {:node (let-with-table-variables
-               (do-form (cons facts bindings))
-               (map hooks/token-node (hooks/sexpr vec-bindings)))}
+      {:node (->> (hooks/sexpr vec-bindings)
+                  (map hooks/token-node)
+                  (let-form body))}
       {:node (->> (cons vec-bindings bindings)
                   (filter table-variable?)
-                  (let-with-table-variables facts))})))
+                  (let-form body))})))
+
+(defn ^:private handle-fact-inside-tabular [children]
+  (if (hooks/string-node? (first children))
+    (let [[_name fact vec-bindings & bindings] children]
+      (fact-tabular fact vec-bindings bindings))
+    (let [[fact vec-bindings & bindings] children]
+      (fact-tabular fact vec-bindings bindings))))
+
+(defn tabular [{:keys [node]}]
+  (let [children (rest (:children node))
+        fact-outside (first (filter #(contains? arrows (hooks/sexpr %)) children))]
+    (if fact-outside
+      (handle-fact-outside-tabular children fact-outside)
+      (handle-fact-inside-tabular children))))
