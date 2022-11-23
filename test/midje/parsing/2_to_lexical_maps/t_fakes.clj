f4beecad69b3b20f87b66dac323ccae63723f384
Phillip Mates <phillipmates@gmail.com>
2022-11-23 17:19:37 +0100

    Disallow mocking clojure.core/str (#485)
    * add failing test for when you try to mock `str`
    * prevent mocking `clojure.core/str`

--- test/midje/parsing/2_to_lexical_maps/t_fakes.clj
+++ test/midje/parsing/2_to_lexical_maps/t_fakes.clj
@@ -17,3 +17,17 @@
   (provided
     (set [1]) => true))
 (note-that (fact-captured-throwable-with-message #"You seem to have created a prerequisite*"))
+
+
+(defn noop [y] y)
+(defn my-fn [x] (noop (str x)))
+
+(silent-fact "results in stack overflow"
+  (my-fn 'url) => 'some-result
+  (provided (noop (str 'url)) => 'some-result))
+(note-that (fact-captured-throwable-with-message #"You seem to have created a prerequisite*"))
+
+(fact "note that you can get around it by not mocking `str`"
+  (let [s (str 'url)]
+    (my-fn 'url) => 'some-result
+    (provided (noop s) => 'some-result)))
